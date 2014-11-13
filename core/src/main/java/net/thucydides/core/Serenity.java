fe1c3c5eb2cee95bcc3fc17f0008589f80b16dd0
John Ferguson Smart <john.smart@wakaleo.com>
2014-11-13 08:27:09 +0100

    Added the Serenity utility class, which exposes and delegates to methods of the legacy Thucydides class.

---
+++ core/src/main/java/net/thucydides/core/Serenity.java
@@ -0,0 +1,86 @@
+package net.thucydides.core;
+
+import net.thucydides.core.steps.StepListener;
+import org.openqa.selenium.firefox.FirefoxProfile;
+
+/**
+ * A utility class that provides services to initialize web testing and reporting-related fields in arbitrary objects.
+ * It wraps calls to the legacy Thucydides class.
+ */
+
+public class Serenity {
+
+    public static void initialize(final Object testCase) {
+        Thucydides.initialize(testCase);
+    }
+
+    /**
+     * Initialize Thucydides-related fields in the specified object.
+     * This includes managed WebDriver instances,
+     *
+     * @param testCase any object (testcase or other) containing injectable Thucydides components
+     */
+    public static void initializeWithNoStepListener(final Object testCase) {
+        Thucydides.initializeWithNoStepListener(testCase);
+    }
+
+    /**
+     * Instantiates the @ManagedPages-annotated Pages instance using current WebDriver.
+     *
+     * @param testCase any object (testcase or other) containing injectable Thucydides components
+     */
+    public static void injectScenarioStepsInto(final Object testCase) {
+        Thucydides.injectScenarioStepsInto(testCase);
+    }
+
+    /**
+     * Indicate that the test run using this object is finished, and reports can be generated.
+     */
+    public static void done() {
+        Thucydides.done();
+    }
+
+    public static String getCurrentSessionID() {
+        return Thucydides.getCurrentSessionID();
+    }
+
+    public static StepListener getStepListener() {
+        return Thucydides.getStepListener();
+    }
+
+    public static void initializeTestSession() {
+        Thucydides.initializeTestSession();
+    }
+
+    public static SessionMap<Object, Object> getCurrentSession() {
+        return Thucydides.getCurrentSession();
+    }
+
+    public static void pendingStep(String reason) {
+        Thucydides.pendingStep(reason);
+    }
+
+    public static void ignoredStep(String reason) {
+        Thucydides.ignoredStep(reason);
+    }
+
+    public static void takeScreenshot() {
+        Thucydides.takeScreenshot();
+    }
+
+    /**
+     * @return The current working directory name is used as a default project key if no other key is provided.
+     */
+    public static String getDefaultProjectKey() {
+        return Thucydides.getDefaultProjectKey();
+    }
+
+    public static void useFirefoxProfile(FirefoxProfile profile) {
+        Thucydides.useFirefoxProfile(profile);
+    }
+
+    public static FirefoxProfile getFirefoxProfile() {
+        return Thucydides.getFirefoxProfile();
+    }
+
+}

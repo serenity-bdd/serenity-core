3705ee4ffed330e6063fc70ef085a188ca760a1b
Mike Wilkes <mikezx6r@gmail.com>
2014-11-26 13:52:34 -0500

    [rename] Create Serenity namespaced class, deprecate Thucydides version and delegate functions

--- core/src/test/groovy/net/serenity_bdd/core/reports/WhenSettingUpReportServices.groovy
+++ core/src/test/groovy/net/serenity_bdd/core/reports/WhenSettingUpReportServices.groovy
@@ -1,6 +1,6 @@
-package net.thucydides.core.reports
+package net.serenity_bdd.core.reports
 
-import net.thucydides.core.ThucydidesReports
+import net.serenity_bdd.core.SerenityReports
 import net.thucydides.core.util.MockEnvironmentVariables
 import net.thucydides.core.webdriver.SystemPropertiesConfiguration
 import org.openqa.selenium.WebDriver
@@ -13,14 +13,14 @@ class WhenSettingUpReportServices extends Specification {
 
     def "should be able to configure default report services"() {
         when:
-            def reportService = ThucydidesReports.getReportService(configuration)
+            def reportService = SerenityReports.getReportService(configuration)
         then:
             reportService.getSubscribedReporters().size() != 0
     }
 
     def "should be able to configure default listeners"() {
         when:
-            def listeners = ThucydidesReports.setupListeners(configuration)
+            def listeners = SerenityReports.setupListeners(configuration)
         then:
             listeners.baseStepListener != null
     }
@@ -28,14 +28,14 @@ class WhenSettingUpReportServices extends Specification {
     def "should be able to configure default listeners with a webdriver instance"() {
         when:
             def driver = Mock(WebDriver)
-            def listeners = ThucydidesReports.setupListeners(configuration).withDriver(driver)
+            def listeners = SerenityReports.setupListeners(configuration).withDriver(driver)
         then:
             listeners.baseStepListener.driver == driver
     }
 
     def "should be able to obtain the results from the base step listener"() {
         when:
-            def listeners = ThucydidesReports.setupListeners(configuration)
+            def listeners = SerenityReports.setupListeners(configuration)
         then:
             listeners.getResults() == listeners.baseStepListener.testOutcomes
     }

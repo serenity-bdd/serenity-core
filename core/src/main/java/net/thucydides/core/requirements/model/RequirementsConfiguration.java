ba4153da4c89e33adb84d40de57906a82f01ba32
John Ferguson Smart <john.smart@wakaleo.com>
2015-05-02 08:23:58 +1000

    Improved automatic detection of file-system requirements hierarchies.
    Looks for JBehave or Cucumber feature files and derives the
    corresponding requirements structure based on the depth of the
    requirements directories.

--- core/src/main/java/net/thucydides/core/requirements/model/RequirementsConfiguration.java
+++ core/src/main/java/net/thucydides/core/requirements/model/RequirementsConfiguration.java
@@ -1,12 +1,20 @@
 package net.thucydides.core.requirements.model;
 
+import com.google.common.base.Optional;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import net.thucydides.core.ThucydidesSystemProperty;
+import net.thucydides.core.requirements.SearchForFilesOfType;
 import net.thucydides.core.util.EnvironmentVariables;
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.List;
 
@@ -16,18 +24,24 @@ public class RequirementsConfiguration {
 
     private EnvironmentVariables environmentVariables;
 
+
+    // Used for testing at this stage
+    private String rootRequirementsDirectory = "/";
+
     public RequirementsConfiguration(EnvironmentVariables environmentVariables) {
         this.environmentVariables = environmentVariables;
     }
 
     public List<String> getRequirementTypes() {
         String requirementTypes = ThucydidesSystemProperty.THUCYDIDES_REQUIREMENT_TYPES.from(environmentVariables);
+        List<String> types;
         if (StringUtils.isNotEmpty(requirementTypes)) {
-            Iterator<String> types = Splitter.on(",").trimResults().split(requirementTypes).iterator();
-            return Lists.newArrayList(types);
+            Iterator<String> typeValues = Splitter.on(",").trimResults().split(requirementTypes).iterator();
+            types = Lists.newArrayList(typeValues);
         } else {
-            return DEFAULT_CAPABILITY_TYPES;
+            types = getDefaultCapabilityTypes();
         }
+        return types;
     }
 
     public String getDefaultRootDirectory() {
@@ -36,4 +50,98 @@ public class RequirementsConfiguration {
         }
         return DEFAULT_ROOT_DIRECTORY;
     }
+
+    public List<String> getDefaultCapabilityTypes() {
+        if (jbehaveFilesExist()) {
+            return jbehaveCapabilityTypes();
+        }
+        if (cucumberFilesExist()) {
+            return cucumberCapabilityTypes();
+        }
+
+        return DEFAULT_CAPABILITY_TYPES;
+    }
+
+    private List<String> jbehaveCapabilityTypes() {
+        int featureDirectoryDepth = getJBehaveFileMatcher().get().getMaxDepth();
+        switch (featureDirectoryDepth) {
+            case 0: return ImmutableList.of("story");
+            case 1: return ImmutableList.of("feature", "story");
+            default: return ImmutableList.of("capability","feature", "story");
+        }
+    }
+
+    private List<String> cucumberCapabilityTypes() {
+        int featureDirectoryDepth = getCucumberFileMatcher().get().getMaxDepth();
+        switch (featureDirectoryDepth) {
+            case 0: return ImmutableList.of("feature");
+            case 1: return ImmutableList.of("capability", "feature");
+            default: return ImmutableList.of("theme","capability", "feature");
+        }
+    }
+
+    SearchForFilesOfType jbehaveFileMatcher;
+
+    private Optional<SearchForFilesOfType> getJBehaveFileMatcher() {
+        if (jbehaveFileMatcher != null) {
+            return Optional.of(jbehaveFileMatcher);
+        }
+        try {
+            Path root = findResourcePath(rootRequirementsDirectory + "/stories");
+            if (root != null) {
+                jbehaveFileMatcher = new SearchForFilesOfType(root,".story");
+                Files.walkFileTree(root, jbehaveFileMatcher);
+                return Optional.of(jbehaveFileMatcher);
+            }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return Optional.absent();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Optional.absent();
+        }
+        return Optional.absent();
+    }
+
+    private Path findResourcePath(String path) throws URISyntaxException {
+        Path root;
+        URL storyDirectory = getClass().getResource(path);
+        if (storyDirectory != null) {
+            root =  Paths.get(storyDirectory.toURI());
+        } else {
+            root = Paths.get("src/test/resources/" + path);
+        }
+        return root;
+    }
+    SearchForFilesOfType cucumberFileMatcher;
+
+    private Optional<SearchForFilesOfType> getCucumberFileMatcher() {
+        if (cucumberFileMatcher != null) {
+            return Optional.of(cucumberFileMatcher);
+        }
+        try {
+            Path root = findResourcePath(rootRequirementsDirectory + "/feature");
+            if (root != null) {
+                cucumberFileMatcher = new SearchForFilesOfType(root,".feature");
+                Files.walkFileTree(root, cucumberFileMatcher);
+                return Optional.of(cucumberFileMatcher);
+            }
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return Optional.absent();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Optional.absent();
+        }
+        return Optional.absent();
+    }
+
+    private boolean jbehaveFilesExist() {
+        return (getJBehaveFileMatcher().isPresent() && getJBehaveFileMatcher().get().hasMatchingFiles());
+    }
+
+    private boolean cucumberFilesExist() {
+        return (getCucumberFileMatcher().isPresent() && getCucumberFileMatcher().get().hasMatchingFiles());
+    }
+
 }
---
+++ core/src/test/resources/one-level-story-files/stories/veges/PlantPotatoes.story
@@ -0,0 +1,14 @@
+GivenStories: GivenThereIsAGarden.story
+
+Narrative:
+As a farmer
+I want to plant potatoes
+So that I can harvest them later on
+
+Meta:
+@Versions Release 1.0, Iteration 1.3
+
+Scenario: scenario description
+Given a system state
+When I do something
+Then system is in a different state

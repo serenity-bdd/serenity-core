a2d03c8dbf1fcb06753274bcdc190badfaabd12d
John Ferguson Smart <john.smart@wakaleo.com>
2015-05-27 18:57:06 +0800

    Fix for THUCYDIDES-253

--- serenity-core/src/main/java/net/thucydides/core/webdriver/ProvidedDriver.java
+++ serenity-core/src/main/java/net/thucydides/core/webdriver/ProvidedDriver.java
@@ -1,5 +1,6 @@
 package net.thucydides.core.webdriver;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 
-public abstract class ProvidedDriver implements WebDriver {}
+public abstract class ProvidedDriver implements WebDriver, JavascriptExecutor {}

14e924da422d639a205ec672a8a6e7ed4ed8455f
willhuang85 <willhuang85@gmail.com>
2015-10-22 04:11:01 -0500

    Revert "Better way to fix THUCYDIDES-253"

--- serenity-core/src/main/java/net/thucydides/core/webdriver/WebDriverFacade.java
+++ serenity-core/src/main/java/net/thucydides/core/webdriver/WebDriverFacade.java
@@ -91,9 +91,6 @@ public class WebDriverFacade implements WebDriver, TakesScreenshot, HasInputDevi
     }
 
     public Class<? extends WebDriver>  getDriverClass() {
-        if (driverClass.isAssignableFrom(SupportedWebDriver.PROVIDED.getWebdriverClass())) {
-            return getProxiedDriver().getClass();
-        }
         return driverClass;
     }
 

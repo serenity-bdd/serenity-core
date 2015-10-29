356fbb9f80c6789750c93bea253bc87ef7295799
YamStranger <YamStranger@bk.ru>
2015-10-29 16:28:41 +0200

    130_issue: added test and default project for gradle plugin

--- serenity-gradle-plugin/src/test/groovy/net/serenitybdd/plugins/gradle/WhenUsingTheGradlePlugin.groovy
+++ serenity-gradle-plugin/src/test/groovy/net/serenitybdd/plugins/gradle/WhenUsingTheGradlePlugin.groovy
@@ -3,6 +3,8 @@ package net.serenitybdd.plugins.gradle
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import spock.lang.Specification
+import net.thucydides.core.util.TestResources
+import java.nio.file.Files
 
 
 /**
@@ -40,6 +42,31 @@ class WhenUsingTheGradlePlugin extends Specification {
         project.tasks.checkOutcomes
     }
 
-
-
+    def "should build aggregate build of simple project with defsults"() {
+        given:
+            def testProject = TestResources
+                .directoryInClasspathCalled("simple-gradle-project")
+            def project = ProjectBuilder.builder().withProjectDir(testProject)
+                .build()
+            def plugin = new SerenityPlugin()
+            def reports = project.projectDir.toPath().resolve("target")
+                .resolve("site").resolve("serenity")
+        when:
+            project.apply plugin: 'java'
+            plugin.apply(project)
+            project.getTasksByName("clearReports", false).first().execute()
+            project.getTasksByName("clean", false).first().execute()
+            project.getTasksByName("compileJava", false).first().execute()
+            project.getTasksByName("processResources", false).first().execute()
+            project.getTasksByName("classes", false).first().execute()
+            project.getTasksByName("assemble", false).first().execute()
+            project.getTasksByName("compileTestJava", false).first().execute()
+            project.getTasksByName("processTestResources", false).first().execute()
+            project.getTasksByName("testClasses", false).first().execute()
+            project.getTasksByName("test", false).first().execute()
+            project.getTasksByName("aggregate", false).first().execute()
+        then:
+            Files.exists(reports)
+            Files.isDirectory(reports)
+    }
 }

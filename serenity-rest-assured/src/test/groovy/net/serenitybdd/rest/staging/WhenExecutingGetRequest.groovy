cd93fbb84276728c5703231817df14edc1a39091
YamStranger <YamStranger@bk.ru>
2016-03-22 23:35:45 +0200

    test: updated tests to check how SerenityRest works with expectation and validations like then, expect, statusCode etc

--- serenity-rest-assured/src/test/groovy/net/serenitybdd/rest/staging/WhenExecutingGetRequest.groovy
+++ serenity-rest-assured/src/test/groovy/net/serenitybdd/rest/staging/WhenExecutingGetRequest.groovy
@@ -5,9 +5,11 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule
 import com.jayway.restassured.RestAssured
 import com.jayway.restassured.specification.RequestSender
 import net.serenitybdd.rest.staging.decorators.RequestSpecificationDecorated
+import net.serenitybdd.rest.staging.decorators.ResponseDecorated
 import net.serenitybdd.rest.staging.decorators.ResponseSpecificationDecorated
 import net.serenitybdd.rest.staging.rules.RestConfigurationAction
 import net.serenitybdd.rest.staging.rules.RestConfigurationRule
+import org.hamcrest.Matchers
 import org.junit.Rule
 import spock.lang.Specification
 
@@ -56,5 +58,137 @@ class WhenExecutingGetRequest extends Specification {
             def generated = sender.get(url)
         then: "created response should be decorated"
             1 * request.get((String) _, (Object) _)
+    }
+
+    def "should return wrapped response during GET by URL called from request"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>2</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/keyboard"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching(path))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(256)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = given().get(url)
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(256)
+    }
+
+    def "should return wrapped response during GET by URL with MAP parameters called from request"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>3</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/house"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(700)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = given().get("$url?status={status}", ["status": "available"])
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(700)
+    }
+
+    def "should return wrapped response during GET by URL with array parameters called from request"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>4</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/pet"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(845)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = given().get("$url?status={status}", "available")
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(845)
+    }
+
+    def "should return wrapped response during GET by URL called from response"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>5</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/child"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching(path))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(945)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = expect().get(url)
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(945)
+    }
+
+    def "should return wrapped response during GET by URL with MAP parameters called from response"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>6</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/book"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(203)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = expect().get("$url?status={status}", ["status": "available"])
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(203)
+    }
+
+    def "should return wrapped response during GET by URL with array parameters called from response"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>7</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/creature"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(506)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = expect().get("$url?status={status}", "available")
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(506)
     }
 }
---
+++ serenity-rest-assured/src/test/groovy/net/serenitybdd/rest/staging/WhenValidatingResponseFromGetOperation.groovy
@@ -0,0 +1,81 @@
+package net.serenitybdd.rest.staging
+
+import com.github.tomakehurst.wiremock.client.WireMock
+import com.github.tomakehurst.wiremock.junit.WireMockRule
+import com.jayway.restassured.specification.RequestSender
+import net.serenitybdd.rest.staging.decorators.RequestSpecificationDecorated
+import net.serenitybdd.rest.staging.decorators.ResponseDecorated
+import net.serenitybdd.rest.staging.decorators.ResponseSpecificationDecorated
+import net.serenitybdd.rest.staging.rules.RestConfigurationAction
+import net.serenitybdd.rest.staging.rules.RestConfigurationRule
+import org.hamcrest.Matchers
+import org.junit.Rule
+import spock.lang.Specification
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*
+import static net.serenitybdd.rest.staging.SerenityRest.reset
+import static net.serenitybdd.rest.staging.SerenityRest.given
+import static net.serenitybdd.rest.staging.SerenityRest.expect
+
+/**
+ * User: YamStranger
+ * Date: 3/14/16
+ * Time: 9:57 AM
+ */
+class WhenValidatingResponseFromGetOperation extends Specification {
+
+    @Rule
+    def WireMockRule wire = new WireMockRule(0);
+
+    @Rule
+    def RestConfigurationRule rule = new RestConfigurationRule(new RestConfigurationAction() {
+        @Override
+        void apply() {
+            reset()
+        }
+    },)
+
+    def "should be possible to validate status code"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>7</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/creature"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(506)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating new request and making get request"
+            def response = given().get(url)
+        then: "created response should be decorated"
+            response instanceof ResponseDecorated
+        and: "returned status should be correct"
+            response.then().statusCode(506)
+    }
+
+    def "should be possible to validate response body"() {
+        given: "configured access point"
+            def body = "<root>" +
+                "<value>7</value>" +
+                "</root>"
+            def base = "http://localhost:${wire.port()}"
+            def path = "/test/creature"
+            def url = "$base$path"
+            stubFor(WireMock.get(urlMatching("$path.*"))
+                .withRequestBody(matching(".*"))
+                .willReturn(aResponse()
+                .withStatus(506)
+                .withHeader("Content-Type", "application/xml")
+                .withBody(body)));
+        when: "creating expectation"
+            def expectation = expect().
+                statusCode(506).
+                body(Matchers.equalTo(body))
+        then: "validation of expectation should be correct"
+            expectation.when().get(url);
+    }
+}

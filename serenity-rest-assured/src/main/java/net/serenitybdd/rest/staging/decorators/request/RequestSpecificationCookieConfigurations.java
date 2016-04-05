5b6c531b8551b464772a205b8d2cae9bb4f345f5
YamStranger <YamStranger@bk.ru>
2016-04-05 19:24:54 +0300

    feat: implemented wrapping of request after configuring cookies Now serenity rest will work correctly after operations like below: ``` given().cookies(mapWithCookies).get(url) given().cookies("value").get(url) given().cookies("value", param).get(url) ```

--- serenity-rest-assured/src/main/java/net/serenitybdd/rest/staging/decorators/request/RequestSpecificationCookieConfigurations.java
+++ serenity-rest-assured/src/main/java/net/serenitybdd/rest/staging/decorators/request/RequestSpecificationCookieConfigurations.java
@@ -1,6 +1,8 @@
 package net.serenitybdd.rest.staging.decorators.request;
 
+import com.jayway.restassured.internal.MapCreator;
 import com.jayway.restassured.internal.RequestSpecificationImpl;
+import com.jayway.restassured.internal.log.LogRepository;
 import com.jayway.restassured.response.Cookie;
 import com.jayway.restassured.response.Cookies;
 import com.jayway.restassured.specification.FilterableRequestSpecification;
@@ -8,7 +10,13 @@ import com.jayway.restassured.specification.RequestSpecification;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+
+import static com.jayway.restassured.internal.assertion.AssertParameter.notNull;
+import static java.util.Arrays.asList;
 
 /**
  * User: YamStranger
@@ -30,31 +38,57 @@ abstract class RequestSpecificationCookieConfigurations extends RequestSpecifica
 
     @Override
     public RequestSpecification cookies(Map<String, ?> cookies) {
-        return core.cookies(cookies);
+        notNull(cookies, "cookies");
+        List<Cookie> list = new LinkedList<>();
+        for (Map.Entry<String, ?> cookie : cookies.entrySet()) {
+            list.add(new Cookie.Builder(cookie.getKey(),
+                    serializeIfNeeded(cookie.getValue())).build());
+        }
+        return cookies(new Cookies(list));
     }
 
     @Override
     public RequestSpecification cookies(Cookies cookies) {
-        return core.cookies(cookies);
+        notNull(cookies, "cookies");
+        core.cookies(cookies);
+        return this;
     }
 
     @Override
     public RequestSpecification cookie(String cookieName, Object value, Object... additionalValues) {
-        return core.cookie(cookieName, value, additionalValues);
+        List<Cookie> list = new LinkedList<>();
+        list.add(new Cookie.Builder(cookieName, serializeIfNeeded(value)).build());
+        for (Object additionalValue : additionalValues) {
+            list.add(new Cookie.Builder(cookieName, serializeIfNeeded(additionalValue)).build());
+        }
+        return cookies(new Cookies(list));
     }
 
     @Override
     public RequestSpecification cookie(String cookieName) {
-        return core.cookie(cookieName);
+        return cookie(cookieName, null);
     }
 
     @Override
     public RequestSpecification cookie(Cookie cookie) {
-        return core.cookie(cookie);
+        notNull(cookie, "Cookie");
+        return cookies(new Cookies(asList(cookie)));
     }
 
     @Override
     public RequestSpecification cookies(String firstCookieName, Object firstCookieValue, Object... cookieNameValuePairs) {
-        return core.cookies(firstCookieName, firstCookieValue, cookieNameValuePairs);
+        return cookies(MapCreator.createMapFromParams(firstCookieName, firstCookieValue, cookieNameValuePairs));
+    }
+
+    protected String serializeIfNeeded(final Object object) {
+        if (object == null) {
+            return null;
+        }
+        try {
+            return (String) this.helper.executeFunction("serializeIfNeeded", new Class[]{Object.class}, object);
+        } catch (Exception e) {
+            throw new IllegalStateException
+                    ("Can not execute serializeIfNeeded from request, SerenityRest can work incorrectly", e);
+        }
     }
 }

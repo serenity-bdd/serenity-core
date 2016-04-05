f3ab690a4ab25aff09a94c782d05f503c333ad96
YamStranger <YamStranger@bk.ru>
2016-04-05 19:25:32 +0300

    adding reflection helper

--- serenity-rest-assured/src/main/java/net/serenitybdd/rest/staging/decorators/ReflectionHelper.java
+++ serenity-rest-assured/src/main/java/net/serenitybdd/rest/staging/decorators/ReflectionHelper.java
@@ -1,6 +1,8 @@
 package net.serenitybdd.rest.staging.decorators;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * User: YamStranger
@@ -28,5 +30,12 @@ public class ReflectionHelper<T> {
             reference.setAccessible(true);
         }
         reference.set(this.object, value);
+    }
+
+    public Object executeFunction(final String function, final Class[] types, final Object... params)
+            throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        Method method = this.object.getClass().getDeclaredMethod(function, types);
+        method.setAccessible(true);
+        return method.invoke(this.object, params.length == 0 ? null : params);
     }
 }

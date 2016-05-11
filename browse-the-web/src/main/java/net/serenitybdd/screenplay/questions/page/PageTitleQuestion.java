cc81c5f3ee8e6dea0c8becc8a7c65a88db4ced2b
John Smart <john.smart@wakaleo.com>
2016-05-11 15:25:53 +0100

    fix:Fixed a reporting issue with the PageTitleQuestion class

--- browse-the-web/src/main/java/net/serenitybdd/screenplay/questions/page/PageTitleQuestion.java
+++ browse-the-web/src/main/java/net/serenitybdd/screenplay/questions/page/PageTitleQuestion.java
@@ -3,7 +3,9 @@ package net.serenitybdd.screenplay.questions.page;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Question;
 import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.annotations.Subject;
 
+@Subject("the title of the page")
 public class PageTitleQuestion implements Question<String> {
     @Override
     public String answeredBy(Actor actor) {

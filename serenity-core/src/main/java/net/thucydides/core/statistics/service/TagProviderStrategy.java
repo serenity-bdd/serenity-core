46ea87b28c5a9ebab3971bae63ea1c7aa9e32e51
John Smart <john.smart@wakaleo.com>
2016-06-24 19:57:48 +0100

    refactor: minor refactoring

--- serenity-core/src/main/java/net/thucydides/core/statistics/service/TagProviderStrategy.java
+++ serenity-core/src/main/java/net/thucydides/core/statistics/service/TagProviderStrategy.java
@@ -2,7 +2,7 @@ package net.thucydides.core.statistics.service;
 
 public interface TagProviderStrategy {
 
-    public boolean canHandleTestSource(String testSource);
+    boolean canHandleTestSource(String testSource);
 
-    public Iterable<TagProvider> getTagProviders();
+    Iterable<TagProvider> getTagProviders();
 }

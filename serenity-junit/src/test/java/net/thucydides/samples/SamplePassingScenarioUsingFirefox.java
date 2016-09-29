3392903d55b7109adf78c4f404022fb8bc0bb7e9
John Smart <john.smart@wakaleo.com>
2016-09-29 15:19:18 +0100

    Use a ChromeDriverService to improve the performance of Chrome tests

--- serenity-junit/src/test/java/net/thucydides/samples/SamplePassingScenarioUsingFirefox.java
+++ serenity-junit/src/test/java/net/thucydides/samples/SamplePassingScenarioUsingFirefox.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.WebDriver;
 @RunWith(ThucydidesRunner.class)
 public class SamplePassingScenarioUsingFirefox {
     
-    @Managed(driver = "chrome")
+    @Managed
     public WebDriver webdriver;
 
     @ManagedPages(defaultUrl = "classpath:static-site/index.html")

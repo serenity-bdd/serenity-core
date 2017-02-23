6ef9f779975d0a97cebea8c219c77ce67a2aa94b
John Smart <john.smart@wakaleo.com>
2017-02-23 21:12:44 +0100

    Removed redundant tests

--- serenity-rest-assured/src/test/groovy/net/serenitybdd/rest/configuring/NextAvailablePort.groovy
+++
@@ -1,8 +0,0 @@
-package net.serenitybdd.rest.configuring
-
-class NextAvailablePort {
-    static int number() {
-        ServerSocket serverSocket = new ServerSocket(0);
-        return serverSocket.getLocalPort();
-    }
-}

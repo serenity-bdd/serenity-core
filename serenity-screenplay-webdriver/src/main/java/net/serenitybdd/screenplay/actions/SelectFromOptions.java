b8b385a070b73f4b561ffadb9edf2582d872f889
kilimandzsaro <natalia.bakos@inbank.ee>
2017-03-22 10:30:12 +0200

    refactor: SelectFromOptions for more readability

--- serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/SelectFromOptions.java
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/SelectFromOptions.java
@@ -12,6 +12,7 @@ public class SelectFromOptions {
 
     private final SelectStrategy strategy;
     private String theText;
+    private String theValue;
     private Integer indexValue;
 
     public SelectFromOptions(SelectStrategy strategy) {
@@ -20,13 +21,13 @@ public class SelectFromOptions {
 
     public static SelectFromOptions byValue(String value) {
         SelectFromOptions selectFromOptions = new SelectFromOptions(SelectStrategy.ByValue);
-        selectFromOptions.theText = value;
+        selectFromOptions.theValue = value;
         return selectFromOptions;
     }
 
-    public static SelectFromOptions byVisibleText(String value) {
+    public static SelectFromOptions byVisibleText(String visibleText) {
         SelectFromOptions selectFromOptions = new SelectFromOptions(SelectStrategy.ByVisibleText);
-        selectFromOptions.theText = value;
+        selectFromOptions.theText = visibleText;
         return selectFromOptions;
     }
 
@@ -42,7 +43,7 @@ public class SelectFromOptions {
 
     public Performable from(Target target) {
         switch (strategy) {
-            case ByValue: return instrumented(SelectByValueFromTarget.class, target, theText);
+            case ByValue: return instrumented(SelectByValueFromTarget.class, target, theValue);
             case ByVisibleText: return instrumented(SelectByVisibleTextFromTarget.class, target, theText);
             case ByIndex: return instrumented(SelectByIndexFromTarget.class, target, indexValue);
         }
@@ -51,7 +52,7 @@ public class SelectFromOptions {
 
     public Performable from(WebElementFacade element) {
         switch (strategy) {
-            case ByValue: return instrumented(SelectByValueFromElement.class, element, theText);
+            case ByValue: return instrumented(SelectByValueFromElement.class, element, theValue);
             case ByVisibleText: return instrumented(SelectByVisibleTextFromElement.class, element, theText);
             case ByIndex: return instrumented(SelectByIndexFromElement.class, element, indexValue);
         }
@@ -60,7 +61,7 @@ public class SelectFromOptions {
 
     public Performable from(By... locators) {
         switch (strategy) {
-            case ByValue: return instrumented(SelectByValueFromBy.class, theText, locators);
+            case ByValue: return instrumented(SelectByValueFromBy.class, theValue, locators);
             case ByVisibleText: return instrumented(SelectByVisibleTextFromBy.class, theText, locators);
             case ByIndex: return instrumented(SelectByIndexFromBy.class, indexValue, locators);
         }

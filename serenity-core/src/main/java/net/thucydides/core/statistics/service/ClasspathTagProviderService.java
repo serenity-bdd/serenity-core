9317d0b41fdcddce1b6bfac3192a0b7727d129e4
John Smart <john.smart@wakaleo.com>
2017-03-22 18:57:05 +0530

    Fixed a bug where tags where not correctly reported
    JUnit tags where not correctly reported when Cucumber scenarios were run before them in the same build.

--- serenity-core/src/main/java/net/thucydides/core/statistics/service/ClasspathTagProviderService.java
+++ serenity-core/src/main/java/net/thucydides/core/statistics/service/ClasspathTagProviderService.java
@@ -1,17 +1,11 @@
 package net.thucydides.core.statistics.service;
 
 import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.ServiceLoader;
 
 public class ClasspathTagProviderService implements TagProviderService {
-
-    private final Logger logger = LoggerFactory.getLogger(ClasspathTagProviderService.class);
-
-    private List<TagProvider> tagProviders;
 
     private TagProviderFilter<TagProvider> filter = new TagProviderFilter<>();
 
@@ -25,15 +19,12 @@ public class ClasspathTagProviderService implements TagProviderService {
 
     @Override
     public List<TagProvider> getTagProviders(String testSource) {
-        if (tagProviders == null) {
             List<TagProvider> newTagProviders = Lists.newArrayList();
             Iterable<? extends TagProvider> tagProviderServiceLoader = loadTagProvidersFromPath(testSource);
             for (TagProvider tagProvider : tagProviderServiceLoader) {
                 newTagProviders.add(tagProvider);
             }
-            tagProviders = filter.removeOverriddenProviders(newTagProviders);
-        }
-        return tagProviders;
+            return filter.removeOverriddenProviders(newTagProviders);
     }
 
     protected Iterable<? extends TagProvider> loadTagProvidersFromPath(String testSource) {

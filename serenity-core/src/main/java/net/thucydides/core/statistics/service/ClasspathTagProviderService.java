b53adb3f578c7433e05ca88cb8447c6e499d7e9f
John Smart <john.smart@wakaleo.com>
2017-04-15 15:09:42 +0100

    Fixed a performance issue with tag management

--- serenity-core/src/main/java/net/thucydides/core/statistics/service/ClasspathTagProviderService.java
+++ serenity-core/src/main/java/net/thucydides/core/statistics/service/ClasspathTagProviderService.java
@@ -1,8 +1,10 @@
 package net.thucydides.core.statistics.service;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ServiceLoader;
 
 public class ClasspathTagProviderService implements TagProviderService {
@@ -17,14 +19,30 @@ public class ClasspathTagProviderService implements TagProviderService {
         return getTagProviders(null);
     }
 
+    private final String ALL_TAG_PROVIDERS = "ALL";
+
+    Map<String, List<TagProvider>> tagProviderCache = Maps.newConcurrentMap();
+
     @Override
     public List<TagProvider> getTagProviders(String testSource) {
-            List<TagProvider> newTagProviders = Lists.newArrayList();
-            Iterable<? extends TagProvider> tagProviderServiceLoader = loadTagProvidersFromPath(testSource);
-            for (TagProvider tagProvider : tagProviderServiceLoader) {
-                newTagProviders.add(tagProvider);
-            }
-            return filter.removeOverriddenProviders(newTagProviders);
+        if (!tagProviderCache.containsKey(forTestSource(testSource))) {
+            tagProviderCache.put(forTestSource(testSource), tagProvidersFor(testSource));
+        }
+        return tagProviderCache.get(forTestSource(testSource));
+
+    }
+
+    private List<TagProvider> tagProvidersFor(String testSource) {
+        List<TagProvider> newTagProviders = Lists.newArrayList();
+        Iterable<? extends TagProvider> tagProviderServiceLoader = loadTagProvidersFromPath(testSource);
+        for (TagProvider tagProvider : tagProviderServiceLoader) {
+            newTagProviders.add(tagProvider);
+        }
+        return filter.removeOverriddenProviders(newTagProviders);
+    }
+
+    private String forTestSource(String testSource) {
+        return (testSource == null) ? ALL_TAG_PROVIDERS : testSource;
     }
 
     protected Iterable<? extends TagProvider> loadTagProvidersFromPath(String testSource) {

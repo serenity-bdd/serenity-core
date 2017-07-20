719475bd0fda7d500f742737e438931b9870697c
John Ferguson Smart <john.smart@wakaleo.com>
2017-07-20 10:56:42 +0100

    Fixed #848 and refactored the Gradle plugin

--- serenity-gradle-plugin/src/main/groovy/net/serenitybdd/plugins/gradle/SerenityPlugin.groovy
+++ serenity-gradle-plugin/src/main/groovy/net/serenitybdd/plugins/gradle/SerenityPlugin.groovy
@@ -10,18 +10,22 @@ import net.thucydides.core.configuration.SystemPropertiesConfiguration
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class SerenityPlugin implements Plugin<Project> {
 
-    File reportDirectory
-    File historyDirectory
+    Path reportDirectory
+    Path historyDirectory
 
     @Override
     void apply(Project project) {
         updateSystemPath(project)
         project.extensions.create("serenity", SerenityPluginExtension)
         project.task('aggregate') {
-            group 'Serenity BDD'
-            description 'Generates aggregated Serenity reports'
+            group = 'Serenity BDD'
+            description = 'Generates aggregated Serenity reports'
             doLast {
                 updateProperties(project)
                 reportDirectory = prepareReportDirectory(project)
@@ -29,21 +33,21 @@ class SerenityPlugin implements Plugin<Project> {
                     project.serenity.projectKey = project.name
                 }
                 logger.lifecycle("Generating Serenity Reports for ${project.serenity.projectKey} to directory $reportDirectory")
-                System.properties['thucydides.project.key'] = project.serenity.projectKey
+                System.properties['serenity.project.key'] = project.serenity.projectKey
                 def reporter = new HtmlAggregateStoryReporter(project.serenity.projectKey)
 
-                reporter.outputDirectory = reportDirectory
+                reporter.outputDirectory = reportDirectory.toFile()
 
                 reporter.issueTrackerUrl = project.serenity.issueTrackerUrl
                 reporter.jiraUrl = project.serenity.jiraUrl
                 reporter.jiraProject = project.serenity.jiraProject
-                reporter.generateReportsForTestResultsFrom(reportDirectory)
+                reporter.generateReportsForTestResultsFrom(reportDirectory.toFile())
             }
         }
 
         project.task('checkOutcomes') {
-            group 'Serenity BDD'
-            description "Checks the Serenity reports and fails the build if there are test failures (run automatically with 'check')"
+            group = 'Serenity BDD'
+            description = "Checks the Serenity reports and fails the build if there are test failures (run automatically with 'check')"
 
             inputs.dir reportDirectory
 
@@ -52,42 +56,44 @@ class SerenityPlugin implements Plugin<Project> {
                 reportDirectory = prepareReportDirectory(project)
                 logger.lifecycle("Checking serenity results for ${project.serenity.projectKey} in directory $reportDirectory")
                 if (reportDirectory.exists()) {
-                    def checker = new ResultChecker(reportDirectory)
+                    def checker = new ResultChecker(reportDirectory.toFile())
                     checker.checkTestResults()
                 }
             }
         }
         project.task('clearReports') {
-            group 'Serenity BDD'
-            description "Deletes the Serenity output directory (run automatically with 'clean')"
+            group = 'Serenity BDD'
+            description = "Deletes the Serenity output directory (run automatically with 'clean')"
 
             doLast {
                 updateProperties(project)
                 reportDirectory = prepareReportDirectory(project)
-                reportDirectory.deleteDir()
+                Files.delete(reportDirectory)
             }
         }
 
         project.task('clearHistory') {
-            group 'Serenity BDD'
-            description "Deletes the Serenity history directory"
+            group = 'Serenity BDD'
+            description = "Deletes the Serenity history directory"
 
             doLast {
                 updateProperties(project)
                 historyDirectory = prepareHistoryDirectory(project)
-                historyDirectory.deleteDir()
+                Files.delete(historyDirectory)
             }
         }
 
         project.task('history') {
-            group 'Serenity BDD'
-            description "Records a summary of test outcomes to be used for comparison in the next test run"
+            group = 'Serenity BDD'
+            description = "Records a summary of test outcomes to be used for comparison in the next test run"
 
             doLast {
                 updateProperties(project)
                 historyDirectory = prepareHistoryDirectory(project)
 
-                new FileSystemTestOutcomeSummaryRecorder(historyDirectory, deletePreviousHistory()).recordOutcomeSummariesFrom(project.serenity.sourceDirectory);
+                new FileSystemTestOutcomeSummaryRecorder(historyDirectory,
+                                                         deletePreviousHistory())
+                        .recordOutcomeSummariesFrom(project.serenity.sourceDirectory);
 
             }
         }
@@ -103,42 +109,40 @@ class SerenityPlugin implements Plugin<Project> {
         }
     }
 
-    def prepareReportDirectory(Project project) {
-        def outputDir = new File(project.serenity.outputDirectory)
+    static def prepareReportDirectory(Project project) {
+        Path outputDir = project.serenity.outputDirectory
         if (!outputDir.isAbsolute()) {
-            outputDir = project.projectDir.toPath().resolve(outputDir.toPath()).toFile()
+            outputDir = project.projectDir.toPath().resolve(outputDir)
         }
-        outputDir
+        return outputDir
     }
 
-    def prepareHistoryDirectory(Project project) {
-        def outputDir = new File(project.serenity.historyDirectory)
+    static Path prepareHistoryDirectory(Project project) {
+        def outputDir = Paths.get(project.serenity.historyDirectory)
         if (!outputDir.isAbsolute()) {
-            outputDir = project.projectDir.toPath().resolve(outputDir.toPath()).toFile()
+            outputDir = project.projectDir.toPath().resolve(outputDir)
         }
-        outputDir
+        return outputDir
     }
 
-    def updateSystemPath(Project project) {
+    static def updateSystemPath(Project project) {
         System.properties['project.build.directory'] = project.projectDir.getAbsolutePath()
-        println ("Updating project.build.directory to ${project.projectDir.getAbsolutePath()}")
-        def SystemPropertiesConfiguration configuration = (SystemPropertiesConfiguration) Injectors.getInjector().getProvider(Configuration.class).get()
+        SystemPropertiesConfiguration configuration = (SystemPropertiesConfiguration) Injectors.getInjector().getProvider(Configuration.class).get()
         configuration.getEnvironmentVariables().setProperty('project.build.directory', project.projectDir.getAbsolutePath())
         configuration.reloadOutputDirectory()
     }
 
-    def deletePreviousHistory() {
-        def SystemPropertiesConfiguration configuration = (SystemPropertiesConfiguration) Injectors.getInjector().getProvider(Configuration.class).get()
-        configuration.environmentVariables.getPropertyAsBoolean(ThucydidesSystemProperty.DELETE_HISTORY_DIRECTORY, true)
+    static Boolean deletePreviousHistory() {
+        SystemPropertiesConfiguration configuration = (SystemPropertiesConfiguration) Injectors.getInjector().getProvider(Configuration.class).get()
+        return configuration.environmentVariables.getPropertyAsBoolean(ThucydidesSystemProperty.DELETE_HISTORY_DIRECTORY, true)
     }
 
 
-    def updateProperties(Project project) {
+    static def updateProperties(Project project) {
         updateSystemPath(project)
         def config = Injectors.getInjector().getProvider(Configuration.class).get()
-        println ("Updating project.serenity.outputDirectory to ${config.getOutputDirectory()}")
-        project.serenity.outputDirectory = config.getOutputDirectory()
-        project.serenity.sourceDirectory = config.getOutputDirectory()
+        project.serenity.outputDirectory = config.getOutputDirectory().toPath()
+        project.serenity.sourceDirectory = config.getOutputDirectory().toPath()
     }
 
 }

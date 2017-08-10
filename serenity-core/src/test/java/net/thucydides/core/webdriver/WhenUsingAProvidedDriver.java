4e87d3bee882e4d4343ac34651ec97cec44a60b2
John Ferguson Smart <john.smart@wakaleo.com>
2017-08-10 08:22:58 +0100

    Updated unit tests

--- serenity-core/src/test/java/net/thucydides/core/webdriver/WhenUsingAProvidedDriver.java
+++ serenity-core/src/test/java/net/thucydides/core/webdriver/WhenUsingAProvidedDriver.java
@@ -1,5 +1,7 @@
 package net.thucydides.core.webdriver;
 
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.MockEnvironmentVariables;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -7,22 +9,36 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
 @RunWith(MockitoJUnitRunner.class)
 public class WhenUsingAProvidedDriver {
 
 
     @Mock
-    WebDriver driver;
-
-    @Mock
     WebDriverFactory factory;
 
     MockWebDriverFacade facade;
 
+    static class MyDriverSource implements DriverSource {
+
+        @Override
+        public WebDriver newDriver() {
+            return new HtmlUnitDriver();
+        }
+
+        @Override
+        public boolean takesScreenshots() {
+            return false;
+        }
+
+        public Class<? extends WebDriver> driverType() { return HtmlUnitDriver.class; }
+
+    }
+
     class MockWebDriverFacade extends WebDriverFacade {
-        MockWebDriverFacade() {
-            super(ProvidedDriver.class, factory);
+        MockWebDriverFacade(EnvironmentVariables environmentVariables) {
+            super(ProvidedDriver.class, factory, environmentVariables);
         }
 
         @Override
@@ -34,11 +50,15 @@ public class WhenUsingAProvidedDriver {
 
     @Before
     public void setup() {
-        facade = new MockWebDriverFacade();
+        EnvironmentVariables environmentVariables = new MockEnvironmentVariables();
+        environmentVariables.setProperty("webdriver.provided.type", "mydriver");
+        environmentVariables.setProperty("webdriver.provided.mydriver", MyDriverSource.class.getName());
+
+        facade = new MockWebDriverFacade(environmentVariables);
     }
 
     @Test
     public void the_web_driver_facade_should_expose_the_proxied_driver_class() {
-        Assert.assertEquals(facade.getDriverClass(), driver.getClass());
+        Assert.assertEquals(facade.getDriverClass(), HtmlUnitDriver.class);
     }
 }

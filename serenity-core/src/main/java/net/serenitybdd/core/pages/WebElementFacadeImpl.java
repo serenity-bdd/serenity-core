bc2adb29986e28f7bdb4adbc4df65fbb0750fe70
John Ferguson Smart <john.smart@wakaleo.com>
2017-10-15 10:20:18 +0100

    Trouble-shooting failing tests on Jenkins

--- serenity-core/src/main/java/net/serenitybdd/core/pages/WebElementFacadeImpl.java
+++ serenity-core/src/main/java/net/serenitybdd/core/pages/WebElementFacadeImpl.java
@@ -906,6 +906,9 @@ public class WebElementFacadeImpl implements WebElementFacade, net.thucydides.co
         } catch (TimeoutException timeout) {
             throw new ElementShouldBeEnabledException("Expected enabled element was not enabled", timeout);
         }
+        if (webElement == null) {
+            throw new ElementShouldBeEnabledException("Expected enabled element was not enabled for " + this.toString());
+        }
         return this;
     }
 
--- serenity-core/src/main/java/net/thucydides/core/webdriver/exceptions/ElementShouldBeEnabledException.java
+++ serenity-core/src/main/java/net/thucydides/core/webdriver/exceptions/ElementShouldBeEnabledException.java
@@ -4,6 +4,9 @@ import net.serenitybdd.core.exceptions.CausesAssertionFailure;
 import org.openqa.selenium.TimeoutException;
 
 public class ElementShouldBeEnabledException extends TimeoutException  implements CausesAssertionFailure {
+    public ElementShouldBeEnabledException(String message) {
+        super(message);
+    }
     public ElementShouldBeEnabledException(String message, Throwable cause) {
         super(message, cause);
     }

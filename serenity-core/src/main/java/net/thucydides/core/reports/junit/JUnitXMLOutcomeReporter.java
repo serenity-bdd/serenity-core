c08ff11552393a29400acb65d92d65752b8a3346
John Ferguson Smart <john.smart@wakaleo.com>
2018-02-08 19:32:50 -0500

    Possible work-around for #1121

--- serenity-core/src/main/java/net/thucydides/core/reports/junit/JUnitXMLOutcomeReporter.java
+++ serenity-core/src/main/java/net/thucydides/core/reports/junit/JUnitXMLOutcomeReporter.java
@@ -1,10 +1,10 @@
 package net.thucydides.core.reports.junit;
 
+import io.vavr.collection.List;
 import net.thucydides.core.model.ReportNamer;
 import net.thucydides.core.model.ReportType;
 import net.thucydides.core.model.TestOutcome;
 import net.thucydides.core.reports.TestOutcomes;
-import net.thucydides.core.reports.io.SafelyMoveFiles;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,12 +14,10 @@ import javax.xml.transform.TransformerException;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-import io.vavr.collection.List;
 
 import static io.vavr.API.List;
 
-public class JUnitXMLOutcomeReporter  {
+public class JUnitXMLOutcomeReporter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JUnitXMLOutcomeReporter.class);
 
@@ -34,39 +32,35 @@ public class JUnitXMLOutcomeReporter  {
         junitXMLConverter = new JUnitXMLConverter();
     }
 
-    public void generateReportsFor(TestOutcomes testOutcomes) throws IOException {
+    public void generateReportsFor(TestOutcomes testOutcomes) {
 
         LOGGER.debug("GENERATING JUNIT REPORTS");
 
-        Map<String, List<TestOutcome>> testOutcomesGroupedByTestCase = groupByTestCase(testOutcomes);
+        groupByTestCase(testOutcomes).entrySet().stream().parallel().forEach(
+                entry -> {
+                    String testCase = entry.getKey();
+                    List<TestOutcome> testCaseOutcomes = entry.getValue();
 
-        for(String testCase : testOutcomesGroupedByTestCase.keySet()) {
-            List<TestOutcome> testCaseOutcomes = testOutcomesGroupedByTestCase.get(testCase);
-            String reportFilename = reportFilenameFor(testCaseOutcomes.get(0));
-            String unique = UUID.randomUUID().toString();
-            File temporary = new File(getOutputDirectory(), reportFilename.concat(unique));
-            File report = new File(getOutputDirectory(), reportFilename);
-            report.createNewFile();
-
-            LOGGER.debug("GENERATING JUNIT REPORT {} using temporary file {}", reportFilename, temporary);
-            try(OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(temporary))){
-                junitXMLConverter.write(testCase, testCaseOutcomes.asJava(), outputStream);
-                outputStream.flush();
-            } catch (ParserConfigurationException | TransformerException e) {
-                throw new IOException(e);
-            }
-            SafelyMoveFiles.withMaxRetriesOf(3).from(temporary.toPath()).to(report.toPath());
-        }
+                    String reportFilename = reportFilenameFor(testCaseOutcomes.get(0));
+                    File report = new File(getOutputDirectory(), reportFilename);
+                    try (OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(report))) {
+                        junitXMLConverter.write(testCase, testCaseOutcomes.asJava(), outputStream);
+                        outputStream.flush();
+                    } catch (ParserConfigurationException | TransformerException | IOException e) {
+                        LOGGER.warn("Failed to generate JUnit XML report", e);
+                    }
+                }
+        );
     }
 
     private String reportFilenameFor(TestOutcome testOutcome) {
         ReportNamer reportNamer = ReportNamer.forReportType(ReportType.XML);
-        return FILE_PREFIX  + reportNamer.getNormalizedTestNameFor(testOutcome);
+        return FILE_PREFIX + reportNamer.getNormalizedTestNameFor(testOutcome);
     }
 
     private Map<String, List<TestOutcome>> groupByTestCase(TestOutcomes testOutcomes) {
-        Map<String, List<TestOutcome>> groupedTestOutcomes = new HashMap();
-        for(TestOutcome outcome : testOutcomes.getOutcomes()) {
+        Map<String, List<TestOutcome>> groupedTestOutcomes = new HashMap<>();
+        for (TestOutcome outcome : testOutcomes.getOutcomes()) {
             String testCaseName = StringUtils.isNotEmpty(outcome.getTestCaseName()) ? outcome.getTestCaseName() : outcome.getStoryTitle();
             List<TestOutcome> currentOutcomes = groupedTestOutcomes.getOrDefault(testCaseName, List());
             groupedTestOutcomes.put(testCaseName, currentOutcomes.append(outcome));

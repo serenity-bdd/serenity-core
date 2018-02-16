serenity-bdd/serenity-core

commit e655282e446bdb3bfbd78aca346577c15f2b8983

Updated to Java 8 and replaced most references to LambdaJ with Java 8 streams.

serenity-core/src/main/java/net/thucydides/core/reports/junit/JUnitXMLConverter.java
@@ -16,11 +16,13 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 import java.io.OutputStream;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Set;
 
 public class JUnitXMLConverter {
 
+    private final static DateTimeFormatter TIMESTAMP_FORMAT = DateTimeFormatter.ofPattern("YYYY-MM-DD hh:mm:ss");
 
     public void write(String testCaseName, List<TestOutcome> outcomes, OutputStream outputStream) throws ParserConfigurationException, TransformerException {
         DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -190,9 +192,11 @@ public class JUnitXMLConverter {
         testSuiteElement.setAttribute("errors", Integer.toString(errors));
         testSuiteElement.setAttribute("skipped", Integer.toString(skipped));
         testSuiteElement.setAttribute("failures", Integer.toString(failures));
-        if (testCaseOutcomes.getStartTime() != null) {
-            testSuiteElement.setAttribute("timestamp", testCaseOutcomes.getStartTime().toString("YYYY-MM-DD hh:mm:ss"));
-        }
+
+        testCaseOutcomes.getStartTime().ifPresent(
+                startTime -> testSuiteElement.setAttribute("timestamp", startTime.toString())
+        );
+
         return testSuiteElement;
     }
 }

commit 71aea536d396b6b435b831c20eca4942f43f69e6

Fixed JUNIT XML generation issue. Wrong timestamp format was generated: for example: "2018-01-09T16:38:20.646+02:00[Europe/Helsinki]"
This caused the report generation to break.

Issue was introduced in commit e655282e446bdb3bfbd78aca346577c15f2b8983
See https://github.com/serenity-bdd/serenity-core/commit/e655282e446bdb3bfbd78aca346577c15f2b8983

Issue is also mentioned in issue https://github.com/serenity-bdd/serenity-core/issues/843, but this was (incorrectly?) closed.

serenity-core/src/main/java/net/thucydides/core/reports/junit/JUnitXMLConverter.java
@@ -194,7 +194,7 @@ public class JUnitXMLConverter {
         testSuiteElement.setAttribute("failures", Integer.toString(failures));
 
         testCaseOutcomes.getStartTime().ifPresent(
-                startTime -> testSuiteElement.setAttribute("timestamp", startTime.toString())
+                startTime -> testSuiteElement.setAttribute("timestamp", TIMESTAMP_FORMAT.format(startTime))
         );
 
         return testSuiteElement;
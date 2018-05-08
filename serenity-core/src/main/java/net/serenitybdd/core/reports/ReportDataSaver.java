1fbf6023bdc5078da1b617e671f7aac97f1a043c
John Ferguson Smart <john.smart@wakaleo.com>
2018-05-08 23:27:04 +0100

    It is now possible to embed custom data in your Serenity reports. This capability has existed for REST testing for some time - the body of the REST queries and responses are included in the Serenity reports - but now you can do the same thing for your own data.
    This feature is quite simple to use. At any point during test execution, you can include data as a string as shown here:
    
    ```
    Serenity.recordReportData().withTitle("Some data").andContents("Some special data")
    ```
    
    If you have a file to include in the output, you can do that too:
    ```
    Path testDataSource = Paths.get("/my/log/file")
    Serenity.recordReportData().withTitle("Output log").fromFile(testDataSource);
    ```

---
+++ serenity-core/src/main/java/net/serenitybdd/core/reports/ReportDataSaver.java
@@ -0,0 +1,52 @@
+package net.serenitybdd.core.reports;
+
+import net.thucydides.core.model.ReportData;
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.steps.StepEventBus;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.util.Optional;
+
+import static net.thucydides.core.ThucydidesSystemProperty.SERENITY_REPORT_ENCODING;
+
+public class ReportDataSaver implements WithTitle, AndContent {
+
+
+    private final StepEventBus eventBus;
+    private String title;
+
+    public ReportDataSaver(StepEventBus eventBus) {
+        this.eventBus = eventBus;
+    }
+
+    public AndContent withTitle(String title) {
+        this.title = title;
+        return this;
+    }
+
+    @Override
+    public void andContents(String contents) {
+        eventBus.getBaseStepListener().latestTestOutcome().ifPresent(
+                outcome -> outcome.currentStep().withReportData(ReportData.withTitle(title).andContents(contents))
+        );
+    }
+
+    @Override
+    public void fromFile(Path source) throws IOException {
+        Charset encoding = Charset.forName(SERENITY_REPORT_ENCODING.from(eventBus.getEnvironmentVariables(), StandardCharsets.UTF_8.name()));
+        fromFile(source, encoding);
+    }
+
+    @Override
+    public void fromFile(Path source, Charset encoding) throws IOException {
+
+        Optional<TestOutcome> outcome = eventBus.getBaseStepListener().latestTestOutcome();
+
+        if (outcome.isPresent()) {
+            outcome.get().currentStep().withReportData(ReportData.withTitle(title).fromFile(source, encoding));
+        }
+    }
+}

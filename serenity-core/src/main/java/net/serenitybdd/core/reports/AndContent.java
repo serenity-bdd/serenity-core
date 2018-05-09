557bee706b22a3d89a966eabc4c62f19d8074806
John Ferguson Smart <john.smart@wakaleo.com>
2018-05-09 08:36:13 +0100

    Fixed visibility issue in the adding report data API

--- serenity-core/src/main/java/net/serenitybdd/core/reports/AndContent.java
+++ serenity-core/src/main/java/net/serenitybdd/core/reports/AndContent.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Path;
 
-interface AndContent {
+public interface AndContent {
     void andContents(String contents);
     void fromFile(Path source) throws IOException;
     void fromFile(Path source, Charset encoding) throws IOException;

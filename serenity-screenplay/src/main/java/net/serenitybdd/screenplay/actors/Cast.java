28613000b7a92638ea5f5af9944a053b70d70028
John Ferguson Smart <john.smart@wakaleo.com>
2018-07-04 19:50:47 -0400

    feat: You can now create arbitrary Casts of actors more easily with predefined abilities for each actor.
    Imagine you have the following Ability class:
    
    ```
    public class Fetch implements Ability {
        private final String item;
    
        private Fetch(String item) {
            this.item = item;
        }
        public static Fetch some(String item) { return new Fetch(item)}
    };
    ```
    
    You can define a cast of actors who have this ability like this:
    ```
            Cast globeTheatreCast = Cast.whereEveryoneCan(Fetch.some("Coffee")));
    ```
    
    Or you can use a lambda expression for more flexibility:
    ```
            Cast globeTheatreCast = Cast.whereEveryoneCan(actor -> actor.whoCan(Fetch.some("Coffee"))));
    ```

--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/actors/Cast.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/actors/Cast.java
@@ -7,11 +7,29 @@ import net.serenitybdd.screenplay.Actor;
 
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * Provide simple support for managing Screenplay actors in Cucumber-JVM or JBehave
  */
 public class Cast {
+
+
+    /**
+     * Create a Cast object with a list of predefined abilities
+     */
+    public static Cast whereEveryoneCan(Ability... abilities) {
+        return new SimpleCast(abilities);
+    }
+
+    /**
+     * Create a Cast object where each actor is configured using the provided function.
+     * E.g.
+     *     Cast globeTheatreCast = Cast.whereEveryoneCan(actor -> actor.whoCan(Fetch.some("Coffee")));
+     */
+    public static Cast whereEveryoneCan(Consumer<Actor>... abilities) {
+        return new SimpleCast(abilities);
+    }
 
     Map<String, Actor> actors = new HashMap();
 

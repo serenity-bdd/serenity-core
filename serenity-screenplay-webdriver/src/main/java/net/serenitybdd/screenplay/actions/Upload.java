0ee932d0a77c1bb52f53007338e11ac54a514040
John Ferguson Smart <john.smart@wakaleo.com>
2018-07-12 20:48:48 -0400

    Added an interaction class to upload files. Usage:     actor.attemptsTo(Upload.theFile(pathToFile).to(SOME_TARGET);

---
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/Upload.java
@@ -0,0 +1,35 @@
+package net.serenitybdd.screenplay.actions;
+
+import net.serenitybdd.screenplay.Interaction;
+import net.serenitybdd.screenplay.Performable;
+import net.serenitybdd.screenplay.targets.Target;
+import org.openqa.selenium.WebElement;
+
+import java.nio.file.Path;
+
+import static net.serenitybdd.screenplay.Tasks.instrumented;
+
+/**
+ * Upload a file from the local filesystem to a webpage using the standard HTML upload form.
+ */
+public class Upload {
+
+    public static UploadBuilder theFile(Path fileToUpload) { return new UploadBuilder(fileToUpload); }
+
+    private static class UploadBuilder {
+        private final Path fileToUpload;
+
+        public UploadBuilder(Path fileToUpload) {
+            this.fileToUpload = fileToUpload;
+        }
+
+        public Performable to(Target uploadField) {
+            return instrumented(UploadToTarget.class, fileToUpload, uploadField);
+        }
+
+        public Performable to(WebElement uploadField) {
+            return instrumented(UploadToWebElement.class, fileToUpload, uploadField);
+        }
+
+    }
+}
---
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/UploadToWebElement.java
@@ -0,0 +1,27 @@
+package net.serenitybdd.screenplay.actions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.targets.Target;
+import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.WebElement;
+
+import java.nio.file.Path;
+
+public class UploadToWebElement extends UploadToField {
+
+    private final WebElement uploadField;
+
+    public UploadToWebElement(Path inputFile, WebElement uploadField) {
+        super(inputFile);
+        this.uploadField = uploadField;
+    }
+
+    @Override
+    @Step("Upload file at #inputFile to #uploadField")
+    public <T extends Actor> void performAs(T actor) {
+        BrowseTheWeb.as(actor).upload(inputFile.toFile().getPath())
+                              .fromLocalMachine()
+                              .to(uploadField);
+    }
+}

90cc8235a3dcc46936b9ed22b3e31821b041ff2d
John Ferguson Smart <john.smart@wakaleo.com>
2018-08-02 06:59:39 +0100

    feature: Added a SendKeys interaction as an alternative to Enter. The Enter interaction waits for the element to be enabled and clears the field before entering the text, whereas SendKeys will just call the Selenium sendKeys method directly.

--- serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/SendKeys.java
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/SendKeys.java
@@ -6,35 +6,31 @@ import org.openqa.selenium.By;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
-public class Enter {
+public class SendKeys {
 
     private final String theText;
 
-    public Enter(String theText) {
+    public SendKeys(String theText) {
         this.theText = theText;
     }
 
-    public static Enter theValue(String text) {
-        return new Enter(text);
-    }
-
-    public static SendKeys keyValues(String text) {
+    public static SendKeys of(String text) {
         return new SendKeys(text);
     }
 
     public EnterValue into(String cssOrXpathForElement) {
-        return instrumented(EnterValueIntoTarget.class, theText, Target.the(cssOrXpathForElement).locatedBy(cssOrXpathForElement));
+        return instrumented(SendKeysIntoTarget.class, theText, Target.the(cssOrXpathForElement).locatedBy(cssOrXpathForElement));
     }
 
     public EnterValue into(Target target) {
-        return instrumented(EnterValueIntoTarget.class, theText, target);
+        return instrumented(SendKeysIntoTarget.class, theText, target);
     }
 
     public EnterValue into(WebElementFacade element) {
-        return instrumented(EnterValueIntoElement.class, theText, element);
+        return instrumented(SendKeystoElement.class, theText, element);
     }
 
     public EnterValue into(By... locators) {
-        return instrumented(EnterValueIntoBy.class, theText, locators);
+        return instrumented(SendKeysIntoBy.class, theText, locators);
     }
 }

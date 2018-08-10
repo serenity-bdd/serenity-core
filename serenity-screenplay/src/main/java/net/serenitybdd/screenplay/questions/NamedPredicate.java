82462fc91039c31c9c684a52a4bc40ec0facfe13
John Ferguson Smart <john.smart@wakaleo.com>
2018-08-10 14:45:53 +0100

    Fixed #1341.
    Java 8 Predicates have no toString() method, and so appear as an object reference in the reports when they are used in Screenplay. This change addes a `returnsAValueThat` method which converts a Predicate to a printable Predicate. E.g.
    
    ```
            then(dana).should(seeThat("Total cost", theTotalCost(),
                                       GivenWhenThen.returnsAValueThat("is equal to 15", isEqual(15))));
    ```

---
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/questions/NamedPredicate.java
@@ -0,0 +1,38 @@
+package net.serenitybdd.screenplay.questions;
+
+import java.util.function.Predicate;
+
+public class NamedPredicate<T> implements Predicate<T>{
+    private final String name;
+    private final Predicate<T> predicate;
+
+    public NamedPredicate(String name, Predicate<T> predicate) {
+        this.name = name;
+        this.predicate = predicate;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean test(T test) {
+        return predicate.test(test);
+    }
+
+    @Override
+    public Predicate<T> and(Predicate<? super T> other) {
+        return new NamedPredicate<T>(name, predicate.and(other));
+    }
+
+    @Override
+    public Predicate<T> negate() {
+        return new NamedPredicate<T>(name, predicate.negate());
+    }
+
+    @Override
+    public Predicate<T> or(Predicate<? super T> other) {
+        return new NamedPredicate<T>(name, predicate.or(other));
+    }
+}

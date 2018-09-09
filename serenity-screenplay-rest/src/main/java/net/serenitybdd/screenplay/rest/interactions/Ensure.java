5e3f0811410f64d59f906081741be4f84ca7f2c2
John Ferguson Smart <john.smart@wakaleo.com>
2018-09-09 19:14:36 +0100

    Refactoring

--- serenity-screenplay-rest/src/main/java/net/serenitybdd/screenplay/rest/interactions/Ensure.java
+++ serenity-screenplay-rest/src/main/java/net/serenitybdd/screenplay/rest/interactions/Ensure.java
@@ -3,13 +3,14 @@ package net.serenitybdd.screenplay.rest.interactions;
 import io.restassured.response.ValidatableResponse;
 import net.serenitybdd.rest.SerenityRest;
 import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Interaction;
 import net.thucydides.core.annotations.Step;
 
 import java.util.function.Consumer;
 
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
-public class Ensure extends RestInteraction {
+public class Ensure implements Interaction {
 
     private final String description;
     private final Consumer<ValidatableResponse> check;
@@ -24,7 +25,6 @@ public class Ensure extends RestInteraction {
     public <T extends Actor> void performAs(T actor) {
         check.accept(SerenityRest.then());
     }
-
 
     public static Ensure that(String description, Consumer<ValidatableResponse> check) {
         return instrumented(Ensure.class, description, check);

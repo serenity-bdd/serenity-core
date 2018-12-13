5765ae5db2c388654d1f4d5a3cca0a8918be8247
John Ferguson Smart <john.smart@wakaleo.com>
2018-12-13 17:48:32 +0000

    Added the SilentTask class as a DSL for non-reportable tasks

--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/SilentTask.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/SilentTask.java
@@ -5,7 +5,7 @@ import net.serenitybdd.core.steps.Instrumented;
 import java.util.Arrays;
 
 public class SilentTask {
-    static <T extends Performable> AnonymousTask where(String title, T... steps) {
+    public static <T extends Performable> AnonymousTask where(String title, T... steps) {
         return Instrumented.instanceOf(AnonymousTask.class).withProperties(title, Arrays.asList(steps)).withNoReporting();
     }
 
--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/conditions/SilentPerformable.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/conditions/SilentPerformable.java
@@ -1,9 +1,10 @@
 package net.serenitybdd.screenplay.conditions;
 
+import net.serenitybdd.markers.IsSilent;
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 
-public class SilentPerformable implements Performable {
+public class SilentPerformable implements Performable, IsSilent {
     @Override
     public <T extends Actor> void performAs(T actor) {}
 }

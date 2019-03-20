e50d7a23e7e11fcc2b723b99e03f50738c02ef1b
John Ferguson Smart <john.smart@wakaleo.com>
2019-03-20 18:48:06 +0000

    Hade the emailable HTML reports work better with Outlook

--- serenity-stats/src/main/kotlin/net/serenitybdd/reports/model/ScenarioSummary.kt
+++ serenity-stats/src/main/kotlin/net/serenitybdd/reports/model/ScenarioSummary.kt
@@ -3,11 +3,12 @@ package net.serenitybdd.reports.model
 import net.thucydides.core.model.TestResult
 
 /**
- * A summary of a failing scenario to appear in the eamil reports
+ * A summary of a failing scenario to appear in the email reports
  */
 class ScenarioSummary(val title: String, testResult: TestResult, val reportName: String, conciseErrorMessage: String) {
     val result = testResult.toString().toLowerCase()
     val errorMessage = removeNoiseFrom(conciseErrorMessage)
+    val color = CSSColor().forResult(testResult)
 
     private fun removeNoiseFrom(conciseErrorMessage: String): String =
             conciseErrorMessage.substringBefore("net.serenitybdd")  // Noisy exceptions

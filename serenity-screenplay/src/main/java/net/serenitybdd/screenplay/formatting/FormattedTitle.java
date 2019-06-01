eaf30094b108d00a69e186ca8bbe0d9b172965d1
John Ferguson Smart <john.smart@wakaleo.com>
2019-06-01 17:10:34 +0100

    Updated a unit test

--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/formatting/FormattedTitle.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/formatting/FormattedTitle.java
@@ -9,10 +9,20 @@ import net.thucydides.core.util.EnvironmentVariables;
 
 public class FormattedTitle {
 
+    private EnvironmentVariables environmentVariables;
+    private Actor actor;
+
+    public FormattedTitle(EnvironmentVariables environmentVariables, Actor actor) {
+        this.environmentVariables = environmentVariables;
+        this.actor = actor;
+    }
 
     public static <T> String ofConsequence(Consequence<T> consequence, Actor actor) {
+        return new FormattedTitle(Injectors.getInjector().getInstance(EnvironmentVariables.class), actor).getFormattedTitleFor(consequence);
+    }
+
+    public <T> String getFormattedTitleFor(Consequence<T> consequence) {
         Formatter formatter = Injectors.getInjector().getInstance(Formatter.class);
-        EnvironmentVariables environmentVariables = Injectors.getInjector().getInstance(EnvironmentVariables.class);
 
         String prefix = "";
         if (ThucydidesSystemProperty.SERENITY_INCLUDE_ACTOR_NAME_IN_CONSEQUENCES.booleanFrom(environmentVariables, false)) {
--- serenity-screenplay/src/test/groovy/net/serenitybdd/screenplay/WhenFormattingConsequenceDescriptions.groovy
+++ serenity-screenplay/src/test/groovy/net/serenitybdd/screenplay/WhenFormattingConsequenceDescriptions.groovy
@@ -1,24 +1,36 @@
 package net.serenitybdd.screenplay
 
 import net.serenitybdd.screenplay.formatting.FormattedTitle
+import net.thucydides.core.util.MockEnvironmentVariables
 import spock.lang.Specification
 
 class WhenFormattingConsequenceDescriptions extends Specification {
 
     def consequence = Mock(Consequence)
+    def actor = Actor.named("Archie")
+    def environmentVariables = new MockEnvironmentVariables()
 
     def "should not modify a simple text"() {
         when:
             consequence.toString() >> "Foo Bar"
         then:
-            FormattedTitle.ofConsequence(consequence) == "Foo Bar"
+            FormattedTitle.ofConsequence(consequence, actor) == "Foo Bar"
     }
 
     def "should make hamcrest expressions HTML-friendly"() {
         when:
         consequence.toString() >> "Then the 'Clear Completed' option should be <Available>"
         then:
-        FormattedTitle.ofConsequence(consequence) == "Then the 'Clear Completed' option should be (Available)"
+        FormattedTitle.ofConsequence(consequence, actor) == "Then the 'Clear Completed' option should be (Available)"
     }
 
+    def "should add actor name if specified"() {
+        given:
+        environmentVariables.setProperty("serenity.include.actor.name.in.consequences","true")
+        when:
+        consequence.toString() >> "Then the 'Clear Completed' option should be <Available>"
+        def formattedTitle = new FormattedTitle(environmentVariables, actor)
+        then:
+        formattedTitle.getFormattedTitleFor(consequence) == "For Archie: Then the 'Clear Completed' option should be (Available)"
+    }
 }

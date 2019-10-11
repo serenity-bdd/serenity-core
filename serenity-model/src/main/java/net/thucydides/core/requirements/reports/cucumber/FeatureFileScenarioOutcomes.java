92c849f7a3d1eafc334e239222e15165e3207de6
liviu.carausu <liviu.carausu@gmail.com>
2019-10-11 18:23:59 +0200

    do not show excluded cucumber examples in report - bug fixed

--- serenity-model/src/main/java/net/thucydides/core/requirements/reports/cucumber/FeatureFileScenarioOutcomes.java
+++ serenity-model/src/main/java/net/thucydides/core/requirements/reports/cucumber/FeatureFileScenarioOutcomes.java
@@ -12,8 +12,14 @@ import net.thucydides.core.reports.html.ReportNameProvider;
 import net.thucydides.core.requirements.model.Requirement;
 import net.thucydides.core.requirements.model.cucumber.AnnotatedFeature;
 import net.thucydides.core.requirements.model.cucumber.CucumberParser;
-import net.thucydides.core.requirements.reports.*;
+import net.thucydides.core.requirements.reports.ReportBadges;
+import net.thucydides.core.requirements.reports.RequirementsOutcomes;
+import net.thucydides.core.requirements.reports.ScenarioOutcome;
+import net.thucydides.core.requirements.reports.ScenarioSummaryOutcome;
+import net.thucydides.core.tags.TagScanner;
 import net.thucydides.core.util.EnvironmentVariables;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.net.URL;
@@ -22,12 +28,17 @@ import java.util.stream.Collectors;
 
 public class FeatureFileScenarioOutcomes {
 
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureFileScenarioOutcomes.class);
+
     private Requirement requirement;
     private EnvironmentVariables environmentVariables;
+    private TagScanner tagScanner;
 
     public FeatureFileScenarioOutcomes(Requirement requirement, EnvironmentVariables environmentVariables) {
         this.requirement = requirement;
         this.environmentVariables = environmentVariables;
+        this.tagScanner = new TagScanner(environmentVariables);
     }
 
     public FeatureFileScenarioOutcomes(Requirement requirement) {
@@ -48,9 +59,9 @@ public class FeatureFileScenarioOutcomes {
             List<ScenarioOutcome> scenarioOutcomes = new ArrayList<>();
             feature.get().getFeature().getChildren().forEach(
                     scenarioDefinition -> scenarioOutcomes.add(
-                                                scenarioOutcomeFrom(feature.get().getFeature(),
-                                                                     scenarioDefinition,
-                                                                     requirementsOutcomes.getTestOutcomes()))
+                            scenarioOutcomeFrom(feature.get().getFeature(),
+                                    scenarioDefinition,
+                                    requirementsOutcomes.getTestOutcomes()))
             );
             return scenarioOutcomes;
         }
@@ -65,7 +76,7 @@ public class FeatureFileScenarioOutcomes {
         String scenarioTitle = scenarioDefinition.getName();
 
         TestResult result = (outcomes.isEmpty()) ? TestResult.UNDEFINED :
-                        TestResultList.overallResultFrom(outcomes.stream().map(TestOutcome::getResult).collect(Collectors.toList()));
+                TestResultList.overallResultFrom(outcomes.stream().map(TestOutcome::getResult).collect(Collectors.toList()));
 
         List<String> reportBadges = ReportBadges.from(outcomes, scenarioDefinition.getName());
 
@@ -73,16 +84,33 @@ public class FeatureFileScenarioOutcomes {
         Optional<String> scenarioReport = (outcomes.isEmpty()) ? Optional.empty() : Optional.of(outcomes.get(0).getHtmlReport());
 
         List<String> renderedSteps = scenarioDefinition.getSteps().stream()
-                    .map(RenderCucumber::step)
-                    .collect(Collectors.toList());
+                .map(RenderCucumber::step)
+                .collect(Collectors.toList());
 
+        List<Examples> filteredExamples = new ArrayList<Examples>();
+        if((scenarioDefinition instanceof ScenarioOutline)) {
+            List<Examples> examples = ((ScenarioOutline) scenarioDefinition).getExamples();
+            for(Examples ex : examples) {
+                if(ex.getTags().size() > 0) {
+                    List<String> alltagNames = ex.getTags().stream().map(Tag::getName).collect(Collectors.toList());
+                    if(tagScanner.shouldRunForTags(alltagNames)){
+                        LOGGER.info("Added example " + ex.getName());
+                        filteredExamples.add(ex);
+                    }
+                } else {
+                    filteredExamples.add(ex);
+                }
+            }
+        }
+
+        List<Examples> examplesList = filteredExamples;
         List<String> renderedExamples = (scenarioDefinition instanceof ScenarioOutline) ?
-                RenderCucumber.examples(((ScenarioOutline) scenarioDefinition).getExamples(),
-                                        feature.getName(),
-                                        scenarioDefinition.getName()) : Collections.EMPTY_LIST;
+                RenderCucumber.examples(examplesList,
+                        feature.getName(),
+                        scenarioDefinition.getName()) : Collections.EMPTY_LIST;
 
         int exampleCount = (scenarioDefinition instanceof ScenarioOutline) ?
-                ((ScenarioOutline) scenarioDefinition).getExamples().stream().mapToInt(examples -> examples.getTableBody().size()).sum()
+                examplesList.stream().mapToInt(examples -> examples.getTableBody().size()).sum()
                 : 0;
 
         Boolean isManual = (outcomes.size() == 1) ? outcomes.get(0).isManual() : hasManualTag(feature.getTags());

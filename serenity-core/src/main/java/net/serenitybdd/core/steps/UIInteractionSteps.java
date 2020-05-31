114b539cd201b13933494e8499c44f2b0d0a17e2
John Ferguson Smart <john.smart@wakaleo.com>
2020-05-31 14:36:59 +0100

    Added the UIQuerySetps action class to make it easier to distinguish classes that act and classes that query

--- serenity-core/src/main/java/net/serenitybdd/core/steps/UIInteractionSteps.java
+++ serenity-core/src/main/java/net/serenitybdd/core/steps/UIInteractionSteps.java
@@ -2,4 +2,7 @@ package net.serenitybdd.core.steps;
 
 import net.serenitybdd.core.pages.PageObject;
 
+/**
+ * An action class designed to interact an application via the UI
+ */
 public class UIInteractionSteps extends PageObject {}
---
+++ serenity-core/src/main/java/net/serenitybdd/core/steps/UIQuerySteps.java
@@ -0,0 +1,8 @@
+package net.serenitybdd.core.steps;
+
+import net.serenitybdd.core.pages.PageObject;
+
+/**
+ * An action class designed to query an application via the UI
+ */
+public class UIQuerySteps extends PageObject {}

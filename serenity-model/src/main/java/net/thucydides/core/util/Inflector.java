f47ba942ba631241fa5f0cecb267bf476fefff4a
Tomasz Wojciechowski <twojciechowski@protonmail.com>
2020-06-04 11:30:16 +0200

    updated link to Inflector class documentation

--- serenity-model/src/main/java/net/thucydides/core/util/Inflector.java
+++ serenity-model/src/main/java/net/thucydides/core/util/Inflector.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 /**
  * Transforms words to singular, plural, humanized (human readable), underscore, camel case, or ordinal form. This is inspired by
- * the <a href="http://api.rubyonrails.org/classes/Inflector.html">Inflector</a> class in <a
+ * the <a href="https://api.rubyonrails.org/classes/ActiveSupport/Inflector.html">Inflector</a> class in <a
  * href="http://www.rubyonrails.org">Ruby on Rails</a>, which is distributed under the <a
  * href="http://wiki.rubyonrails.org/rails/pages/License">Rails license</a>.
  *

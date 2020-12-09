e7c29c558e919c5d52599c9de30ddfc830e77a2b
V. Mark Lehky <mlehky@switch.com>
2020-12-08 16:31:25 -0800

    add new Switch Tasks

---
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/SwitchToNewWindow.java
@@ -0,0 +1,58 @@
+package net.serenitybdd.screenplay.actions;
+
+import java.util.Set;
+
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Task;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.thucydides.core.annotations.Step;
+
+/**
+ * If you just opened a (second?) new window and want to switch to it.
+ */
+public class SwitchToNewWindow implements Task {
+
+    private static final Logger log = LoggerFactory.getLogger(SwitchToNewWindow.class);
+
+    /**
+     * Call this from {@link Switch#toNewWindow()}.
+     */
+    SwitchToNewWindow() { }
+
+    @Override
+    @Step("{0} switches to new window")
+    public <T extends Actor> void performAs(T actor) {
+
+	WebDriver driver = BrowseTheWeb.as(actor).getDriver();
+
+	Set<String> allHandles = driver.getWindowHandles();
+	log.debug("open windows: " + allHandles.size());
+	if (allHandles.size() < 2) {
+	    log.error("Not enough windows open!");
+	    return;
+	}
+
+	String firstHandle = driver.getWindowHandle();
+	Dimension windowSize = driver.manage().window().getSize();
+
+	log.debug("current window: " + driver.getTitle() + ", at: " + driver.getCurrentUrl());
+	for (String handle : allHandles) {
+	    if (!handle.equals(firstHandle)) {
+		driver.switchTo().window(handle);
+		break;
+	    }
+	}
+	log.debug("new window: " + driver.getTitle() + ", at: " + driver.getCurrentUrl());
+
+	// new window is different size when headless
+	if (!driver.manage().window().getSize().equals(windowSize)) {
+	    log.warn("Window size does not match!");
+	    driver.manage().window().setSize(windowSize);
+	}
+    }
+}

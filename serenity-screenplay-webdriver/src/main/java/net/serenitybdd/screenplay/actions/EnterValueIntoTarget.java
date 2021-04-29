7e543086ca8dd5fde8e33d730e7530b724c3a991
Serghei Pogodin <pogodin.serg@gmail.com>
2021-04-29 21:26:54 +0200

    fix: revert back changes related to entering a value into element by using type instead of sendKeys. (#2440)

--- serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/EnterValueIntoTarget.java
+++ serenity-screenplay-webdriver/src/main/java/net/serenitybdd/screenplay/actions/EnterValueIntoTarget.java
@@ -16,7 +16,7 @@ public class EnterValueIntoTarget extends EnterValue {
     @Step("{0} enters #theTextAsAString into #target")
     public <T extends Actor> void performAs(T theUser) {
         textValue().ifPresent(
-                text -> target.resolveFor(theUser).sendKeys(text)
+                text -> target.resolveFor(theUser).type(text)
         );
         if (getFollowedByKeys().length > 0) {
             target.resolveFor(theUser).sendKeys(getFollowedByKeys());

74e6d0f50575b1347d353313fe0c0a5da84b3a2b
Serghei Pogodin <pogodin.serg@gmail.com>
2021-06-15 02:08:18 +0200

    feat: Add erenity-saucelabs module

---
+++ serenity-saucelabs/src/main/java/net/serenitybdd/saucelabs/AfterASauceLabsScenario.java
@@ -0,0 +1,41 @@
+package net.serenitybdd.saucelabs;
+
+import com.google.gson.Gson;
+import net.serenitybdd.core.environment.EnvironmentSpecificConfiguration;
+import net.serenitybdd.core.webdriver.RemoteDriver;
+import net.serenitybdd.core.webdriver.enhancers.AfterAWebdriverScenario;
+import net.thucydides.core.model.ExternalLink;
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.steps.BaseStepListener;
+import net.thucydides.core.util.EnvironmentVariables;
+import org.openqa.selenium.WebDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AfterASauceLabsScenario implements AfterAWebdriverScenario {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(AfterASauceLabsScenario.class);
+
+    @Override
+    public void apply(EnvironmentVariables environmentVariables, TestOutcome testOutcome, WebDriver driver) {
+        if ((driver == null) || (!RemoteDriver.isARemoteDriver(driver))) {
+            return;
+        }
+
+        String sessionId = RemoteDriver.of(driver).getSessionId().toString();
+        String userName = EnvironmentSpecificConfiguration.from(environmentVariables)
+                .getOptionalProperty("saucelabs.user")
+                .orElse(null);
+
+        String key = EnvironmentSpecificConfiguration.from(environmentVariables)
+                .getOptionalProperty("saucelabs.key")
+                .orElse(null);
+
+        SauceLabsTestSession sauceLabsTestSession = new SauceLabsTestSession(userName, key, sessionId);
+        sauceLabsTestSession.updateTestResultFor(testOutcome);
+
+        String publicUrl = sauceLabsTestSession.getPublicUrl();
+        testOutcome.setLink(new ExternalLink(publicUrl, "SauceLabs"));
+    }
+
+}
---
+++ serenity-saucelabs/src/main/java/net/serenitybdd/saucelabs/SauceLabsTestSession.java
@@ -0,0 +1,212 @@
+package net.serenitybdd.saucelabs;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.model.TestResult;
+import org.apache.commons.codec.Charsets;
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+public class SauceLabsTestSession {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SauceLabsTestSession.class);
+    private static final String BROWSER_SESSION_URL = "https://%s:%s@api.eu-central-1.saucelabs.com/rest/v1/%s/jobs/%s";
+    private final String sauceLabsUsername;
+    private final String sauceLabsKey;
+    private final String sessionId;
+    private final Gson gson = new Gson();
+
+    public SauceLabsTestSession(String sauceLabsUsername, String sauceLabsKey, String sessionId) {
+        this.sauceLabsUsername = sauceLabsUsername;
+        this.sauceLabsKey = sauceLabsKey;
+        this.sessionId = sessionId;
+    }
+
+    public void updateTestResultFor(TestOutcome testOutcome) {
+
+        try {
+            HttpPut putRequest = new HttpPut(getSessionUri());
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<>();
+            nameValuePairs.add(new BasicNameValuePair("status", saucelabsCompatibleResultOf(testOutcome)));
+            putRequest.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            HttpClientBuilder.create().build().execute(putRequest);
+        }
+        catch (IOException e) {
+            LOGGER.error("Failed to update SauceLabs results", e);
+        }
+
+    }
+
+    public String getName() {
+        return getSessionProperty("name");
+    }
+
+    public int getDuration() {
+        return Integer.parseInt(getSessionProperty("duration"));
+    }
+
+    public String getOS() {
+        return getSessionProperty("os");
+    }
+
+    public String getOS_Version() {
+        return getSessionProperty("os_version");
+    }
+
+    public String getBrowserVersion() {
+        return getSessionProperty("browser_version");
+    }
+
+    public String getBrowser() {
+        return getSessionProperty("browser");
+    }
+
+    public String getDevice() {
+        return getSessionProperty("device");
+    }
+
+    public String getStatus() {
+        return getSessionProperty("status");
+    }
+
+    public String getHashedId() {
+        return getSessionProperty("hashed_id");
+    }
+
+    public String getReason() {
+        return getSessionProperty("reason");
+    }
+
+    public String getBuildName() {
+        return getSessionProperty("build_name");
+    }
+
+    public String getProjectName() {
+        return getSessionProperty("project_name");
+    }
+
+    public String getTestPriority() {
+        return getSessionProperty("test_priority");
+    }
+
+    public String getLogs() {
+        return getSessionProperty("logs");
+    }
+
+    public String getBrowserstackStatus() {
+        return getSessionProperty("browserstack_status");
+    }
+
+    public String getCreatedAt() {
+        return getSessionProperty("created_at");
+    }
+
+    public String getBrowserUrl() {
+        return getSessionProperty("browser_url");
+    }
+
+    public String getPublicUrl() {
+        return getSessionProperty("public_url");
+    }
+
+    public String getAppiumLogsUrl() {
+        return getSessionProperty("appium_logs_url");
+    }
+
+    public String getVideoUrl() {
+        return getSessionProperty("video_url");
+    }
+
+    public String getBrowserConsoleLogsUrl() {
+        return getSessionProperty("browser_console_logs_url");
+    }
+
+    public String getHarLogsUrl() {
+        return getSessionProperty("har_logs_url");
+    }
+
+    public String getSeleniumLogsUrl() {
+        return getSessionProperty("selenium_logs_url");
+    }
+
+    private String getSessionProperty(String propertyName) {
+        JsonElement sessionElement = null;
+        if ((sauceLabsUsername == null) || (sauceLabsKey == null)) {
+            return null;
+        }
+
+        try {
+            HttpGet querySessionInfo = new HttpGet(getSessionUri());
+            HttpEntity sessionDetails = HttpClientBuilder.create().build().execute(querySessionInfo).getEntity();
+            String sessionBody = EntityUtils.toString(sessionDetails, charsetOf(sessionDetails));
+            sessionElement = gson.fromJson(sessionBody, JsonElement.class);
+        } catch (IOException | JsonSyntaxException e) {
+            LOGGER.error("Failed to connect to SauceLabs API.", e);
+        }
+
+        if (sessionElement == null) {
+            return null;
+        }
+
+        JsonElement automationSession = sessionElement.getAsJsonObject().get("automation_session");
+        return automationSession.getAsJsonObject().get(propertyName).getAsString();
+    }
+
+
+    private Charset charsetOf(HttpEntity entity) {
+        Header encodingHeader = entity.getContentEncoding();
+        return encodingHeader == null ? StandardCharsets.UTF_8 : Charsets.toCharset(encodingHeader.getValue());
+    }
+
+    private String saucelabsCompatibleResultOf(TestOutcome outcome) {
+        switch (latestResultOf(outcome)) {
+            case SUCCESS:
+                return "passed";
+            case FAILURE:
+            case ERROR:
+            case COMPROMISED:
+                return "failed";
+            default:
+                return "complete";
+        }
+    }
+
+
+    private URI getSessionUri() {
+        URI uri = null;
+        try {
+            uri = new URI(String.format(BROWSER_SESSION_URL, sauceLabsUsername, sauceLabsKey, sauceLabsUsername, sessionId));
+        } catch (URISyntaxException e) {
+            LOGGER.error("Failed to parse SauceLabs API url.", e);
+        }
+
+        return uri;
+    }
+
+    private TestResult latestResultOf(TestOutcome outcome) {
+        if (outcome.isDataDriven()) {
+            return outcome.getLatestTopLevelTestStep().get().getResult();
+        } else {
+            return outcome.getResult();
+        }
+    }
+}

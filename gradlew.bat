79fc2802e02da04ff3fec69eb404b0cdbd13c049
josephmillarMC <jmillar@mastercontrol.com>
2021-07-16 08:07:11 -0600

    revert my changes to gradlew

--- gradlew.bat
+++ gradlew.bat
@@ -61,7 +61,7 @@ echo location of your Java installation.
 
 goto fail
 
-nit
+:init
 @rem Get command-line arguments, handling Windows variants
 
 if not "%OS%" == "Windows_NT" goto win9xME_args
@@ -81,10 +81,10 @@ set CMD_LINE_ARGS=%*
 
 set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-
 @rem Execute Gradle
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
 :end
+
 @rem End local scope for the variables with windows NT shell
 if "%ERRORLEVEL%"=="0" goto mainEnd
 

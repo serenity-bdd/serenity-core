61df24145638f6f10057d2af7861823bd507b803
John Ferguson Smart <john.smart@wakaleo.com>
2021-09-08 10:33:53 +0100

    Fixed a bug in the proxy configuration causing a null pointer exception.

--- serenity-screenplay/src/main/java/net/serenitybdd/screenplay/Task.java
+++ serenity-screenplay/src/main/java/net/serenitybdd/screenplay/Task.java
@@ -1,6 +1,7 @@
 package net.serenitybdd.screenplay;
 
 import net.serenitybdd.core.steps.Instrumented;
+import net.thucydides.core.util.NameConverter;
 
 import java.util.Arrays;
 import java.util.function.Consumer;
@@ -11,15 +12,29 @@ import java.util.function.Function;
  * rather than a system interaction.
  */
 public interface Task extends Performable {
+    static <T extends Performable> AnonymousTask where(T... steps) {
+        return Instrumented.instanceOf(AnonymousTask.class).withProperties(HumanReadableTaskName.forCurrentMethod(), Arrays.asList(steps));
+    }
+
     static <T extends Performable> AnonymousTask where(String title, T... steps) {
         return Instrumented.instanceOf(AnonymousTask.class).withProperties(title, Arrays.asList(steps));
+    }
+
+    static <T extends Performable> AnonymousPerformableFunction where(Consumer<Actor> performableOperation) {
+        return Instrumented.instanceOf(AnonymousPerformableFunction.class).withProperties(HumanReadableTaskName.forCurrentMethod(),
+                                                                                          performableOperation);
     }
 
     static <T extends Performable> AnonymousPerformableFunction where(String title, Consumer<Actor> performableOperation) {
         return Instrumented.instanceOf(AnonymousPerformableFunction.class).withProperties(title, performableOperation);
     }
 
+    static <T extends Performable> AnonymousPerformableRunnable thatPerforms(Runnable performableOperation) {
+        return Instrumented.instanceOf(AnonymousPerformableRunnable.class).withProperties(HumanReadableTaskName.forCurrentMethod(), performableOperation);
+    }
+
     static <T extends Performable> AnonymousPerformableRunnable thatPerforms(String title, Runnable performableOperation) {
         return Instrumented.instanceOf(AnonymousPerformableRunnable.class).withProperties(title, performableOperation);
     }
+
 }

357c850fdd8184738876fa5df8055ed417f51074
Liviu Carausu <liviu.carausu@gmail.com>
2021-10-17 00:40:21 +0200

    JUnit5: clearTestOutcomes at baseStepListener after generating report

--- serenity-junit5/src/main/java/net/serenitybdd/junit5/SerenityTestExecutionListener.java
+++ serenity-junit5/src/main/java/net/serenitybdd/junit5/SerenityTestExecutionListener.java
@@ -119,6 +119,7 @@ public class SerenityTestExecutionListener implements TestExecutionListener {
         } else {
             generateReports();
         }
+        baseStepListener.clearTestOutcomes();
     }
 
     @Override

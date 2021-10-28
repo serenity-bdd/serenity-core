2e521ddc6acddb161e7599e6927c153a0e4efc60
John Ferguson Smart <john.smart@wakaleo.com>
2021-10-28 09:39:25 +0100

    Minor build tweaks

--- serenity-cucumber/src/test/groovy/net/serenitybdd/cucumber/web/WhenRunningWebCucumberStories.groovy
+++ serenity-cucumber/src/test/groovy/net/serenitybdd/cucumber/web/WhenRunningWebCucumberStories.groovy
@@ -24,6 +24,43 @@ class WhenRunningWebCucumberStories extends Specification {
         outputDirectory = Files.newTemporaryFolder()
     }
 
+    def "should run table-driven scenarios successfully"() {
+        given:
+        def runtime = serenityRunnerForCucumberTestRunner(SimpleSeleniumScenario.class, outputDirectory, environmentVariables);
+
+        when:
+        runtime.run();
+        def recordedTestOutcomes = new TestOutcomeLoader().forFormat(OutcomeFormat.JSON).loadFrom(outputDirectory);
+        def testOutcome = recordedTestOutcomes[0]
+
+        then:
+        testOutcome.title == "A scenario that uses selenium"
+        testOutcome.tags
+        and: "there should be one step for each row in the table"
+        testOutcome.stepCount == 2
+    }
+
+
+    def "a failing story should generate failure test outcome"() throws Throwable {
+        given:
+        def runtime = serenityRunnerForCucumberTestRunner(SimpleSeleniumFailingScenario.class, outputDirectory, environmentVariables);
+
+        when:
+        runtime.run();
+        def recordedTestOutcomes = new TestOutcomeLoader().forFormat(OutcomeFormat.JSON).loadFrom(outputDirectory);
+        def testOutcome = recordedTestOutcomes[0]
+
+
+        then:
+        testOutcome.title == "A failing scenario that uses selenium"
+        testOutcome.isFailure();
+
+        and: "there should be one step for each row in the table"
+        testOutcome.stepCount == 2
+    }
+
+
+
     def "a test should use a different browser if requested"()  {
         given:
         def runtime = serenityRunnerForCucumberTestRunner(SimpleSeleniumDifferentBrowserScenario.class, outputDirectory);

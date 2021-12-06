a17f1698bb0f19c06e0571ed3ee850d920006f76
John Ferguson Smart <john.smart@wakaleo.com>
2021-12-06 11:06:48 +0000

    Updated to next snapshot version

--- serenity-navigator-report/src/main/kotlin/net/serenitybdd/reports/navigator/SerenityNavigatorReport.java
+++ serenity-navigator-report/src/main/kotlin/net/serenitybdd/reports/navigator/SerenityNavigatorReport.java
@@ -1,6 +1,6 @@
-package net.serenitybdd.reports;
+package net.serenitybdd.reports.navigator;
 
 /**
- * Token Java class for the Javadoc (needed for Maven central)
+ * Token Java class for the Javadoc (needed for Maven central).
  */
 public class SerenityNavigatorReport {}

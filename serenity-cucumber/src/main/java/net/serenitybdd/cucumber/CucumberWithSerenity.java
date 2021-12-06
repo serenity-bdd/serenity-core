48b57c8687c16016c39f53114f455c7b07e7ddc4
John Ferguson Smart <john.smart@wakaleo.com>
2021-12-06 09:58:48 +0000

    Added some javadoc to the Cucumber module

--- serenity-cucumber/src/main/java/net/serenitybdd/cucumber/CucumberWithSerenity.java
+++ serenity-cucumber/src/main/java/net/serenitybdd/cucumber/CucumberWithSerenity.java
@@ -3,6 +3,9 @@ package net.serenitybdd.cucumber;
 import io.cucumber.junit.CucumberSerenityRunner;
 import org.junit.runners.model.InitializationError;
 
+/**
+ * Run Cucumber with Serenity using JUnit 4
+ */
 public class CucumberWithSerenity extends CucumberSerenityRunner {
 
     public CucumberWithSerenity(Class clazz) throws InitializationError {

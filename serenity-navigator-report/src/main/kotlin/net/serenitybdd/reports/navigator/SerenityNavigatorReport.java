89847db0900a6f15ad0af909effde6604451a675
John Ferguson Smart <john.smart@wakaleo.com>
2021-12-06 10:30:03 +0000

    Added a token Java class required by Maven central

--- serenity-navigator-report/src/main/kotlin/net/serenitybdd/reports/navigator/SerenityNavigatorReport.java
+++ serenity-navigator-report/src/main/kotlin/net/serenitybdd/reports/navigator/SerenityNavigatorReport.java
@@ -3,4 +3,4 @@ package net.serenitybdd.reports;
 /**
  * Token Java class for the Javadoc (needed for Maven central)
  */
-public class SerenityNaviatorReport {}
+public class SerenityNavigatorReport {}

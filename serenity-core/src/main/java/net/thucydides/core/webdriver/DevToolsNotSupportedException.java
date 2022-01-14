1914045690de2e5f71ec4a75d33dd8992c6198f7
John Ferguson Smart <john.smart@wakaleo.com>
2022-01-14 09:06:03 +0000

    Added better support for retrieving the DevTools object from the current driver.

---
+++ serenity-core/src/main/java/net/thucydides/core/webdriver/DevToolsNotSupportedException.java
@@ -0,0 +1,7 @@
+package net.thucydides.core.webdriver;
+
+public class DevToolsNotSupportedException extends RuntimeException {
+    public DevToolsNotSupportedException(String message) {
+        super(message);
+    }
+}

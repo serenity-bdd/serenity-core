f338ac86f5d860b44dae169ff7dd295faa9b98fe
John Ferguson Smart <john.smart@wakaleo.com>
2022-01-15 20:06:53 +0000

    PageElements can now be used in Action Classes

---
+++ serenity-screenplay-webdriver/src/test/java/net/serenitybdd/screenplay/webtests/ui/DropdownExamples.java
@@ -0,0 +1,70 @@
+package net.serenitybdd.screenplay.webtests.ui;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.abilities.BrowseTheWeb;
+import net.serenitybdd.screenplay.actions.Click;
+import net.serenitybdd.screenplay.actions.Open;
+import net.serenitybdd.screenplay.questions.Text;
+import net.serenitybdd.screenplay.ui.Dropdown;
+import net.serenitybdd.screenplay.ui.Link;
+import net.thucydides.core.annotations.Managed;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Working with Anchors
+ */
+@RunWith(SerenityRunner.class)
+public class DropdownExamples {
+
+    @Managed(driver = "chrome", options = "--headless")
+    WebDriver driver;
+
+    Actor sarah = Actor.named("Sarah");
+
+    @BeforeClass
+    public static void setupDriver() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @Before
+    public void openBrowser() {
+        sarah.can(BrowseTheWeb.with(driver));
+
+        sarah.attemptsTo(
+                Open.url("classpath:/sample-web-site/screenplay/ui-elements/forms/selects.html")
+        );
+    }
+
+    @Test
+    public void findADropdownValueByName() {
+        sarah.attemptsTo(
+                Click.on(Dropdown.called("cars"))
+        );
+    }
+
+    @Test
+    public void findADropdownValueByLabel() {
+        sarah.attemptsTo(
+                Click.on(Dropdown.withLabel("Choose a car:"))
+        );
+    }
+
+
+    @Test
+    public void findADropdownValueByDefaultOption() {
+        sarah.attemptsTo(
+                Click.on(Dropdown.withDefaultOption("---Pick Your Car---"))
+        );
+    }
+
+
+
+}

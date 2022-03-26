b906de8eb56561d78112c4ec23ce8d5040e6e569
John Ferguson Smart <john.smart@wakaleo.com>
2022-03-26 09:10:53 +0000

    Work-around for #2757 - log a warning if Selenium is unable to take a screenshot for some reason

--- serenity-core/src/main/java/net/serenitybdd/core/photography/WebDriverScreenShooter.java
+++ serenity-core/src/main/java/net/serenitybdd/core/photography/WebDriverScreenShooter.java
@@ -3,6 +3,9 @@ package net.serenitybdd.core.photography;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.ScreenshotException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 
@@ -12,12 +15,19 @@ import java.io.IOException;
 public class WebDriverScreenShooter implements ScreenShooter {
     private final WebDriver driver;
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(WebDriverScreenShooter.class);
+
     public WebDriverScreenShooter(PhotoLens lens) {
         this.driver = ((WebDriverPhotoLens) lens).getDriver();
     }
 
     @Override
     public byte[] takeScreenshot() throws IOException {
-        return (driver instanceof TakesScreenshot) ? ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES) : new byte[]{};
+        try {
+            return (driver instanceof TakesScreenshot) ? ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES) : new byte[]{};
+        } catch(ScreenshotException failedToTakeScreenshot) {
+            LOGGER.warn("Failed to take screenshot - Selenium reported the following error", failedToTakeScreenshot);
+            return new byte[]{};
+        }
     }
 }

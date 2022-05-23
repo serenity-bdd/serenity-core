4dd4d6332fff8f0cb2968099c5bcef744e619d1d
gallardo <argrico@gmail.com>
2022-05-23 18:23:04 +0200

    chore: allow customization of default guice bindings
    Implement `Injectors.setDefaultModule(Module)` to allow setting custom guice binding for the default injector (retrieved via `Injectors.getInjector()`).
    
    Why is this required: The current guice bindings define many singletons. When the serenity-gradle-plugin makes use of these bindings, the singletons are loaded into memory, and because gradle is started by default as a daemon and keeps running in the background, the singletons don't get updated on subsequent invocation of gradle tasks.

--- serenity-model/src/main/java/net/thucydides/core/guice/Injectors.java
+++ serenity-model/src/main/java/net/thucydides/core/guice/Injectors.java
@@ -1,32 +1,47 @@
 package net.thucydides.core.guice;
 
-import com.google.inject.Guice;
-import com.google.inject.Injector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Module;
 
 /**
  * Somewhere to hold the Guice injector.
  * There might be a better way to do this.
  */
 public class Injectors {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Injectors.class);
 
-    private static Map<String,Injector>  injectors = Collections.synchronizedMap(new HashMap<>());
+    private static final Map<String, Injector> injectors = Collections.synchronizedMap(new HashMap<>());
 
+    private static Module defaultModule;
+
+    public static synchronized void setDefaultModule(Module module) {
+        defaultModule = module;
+        LOGGER.trace("Set default guice module {}", module);
+    }
 
     public static synchronized Injector getInjector() {
-        return getInjector(new ThucydidesModule());
+        if (defaultModule == null) {
+            defaultModule = new ThucydidesModule();
+        }
+        return getInjector(defaultModule);
     }
-    
+
     public static synchronized Injector getInjector(com.google.inject.Module module) {
         String moduleClassName = module.getClass().getCanonicalName();
         Injector injector = injectors.get(moduleClassName);
         if (injector == null) {
-    		injector = Guice.createInjector(module);
-    		injectors.put(moduleClassName, injector);
-    	}
-    	return injector;
+            injector = Guice.createInjector(module);
+            injectors.put(moduleClassName, injector);
+            LOGGER.debug("Created injector for module {}", moduleClassName);
+        }
+        return injector;
     }
 }

9f67efd581f5beeea88fc368f812ccb55e062bec
Liviu Carausu <liviu.carausu@gmail.com>
2022-05-30 23:28:19 +0200

    improvements for cucumber parallel runner

--- serenity-cucumber/src/test/java/net/serenitybdd/cucumber/integration/ju5/CucumberTestSuite.java
+++ serenity-cucumber/src/test/java/net/serenitybdd/cucumber/integration/ju5/CucumberTestSuite.java
@@ -1,14 +1,11 @@
 package net.serenitybdd.cucumber.integration.ju5;
 
 
-import io.cucumber.junit.Cucumber;
+
 import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.junit.runner.RunWith;
 
 @Suite
 @IncludeEngines("cucumber")

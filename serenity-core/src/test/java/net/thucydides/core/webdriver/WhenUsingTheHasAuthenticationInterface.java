fdcd47f7bb1ca71f88063cc5035808ccc04a1789
John Ferguson Smart <john.smart@wakaleo.com>
2022-06-15 07:52:33 +0100

    Added support for the Selenium 4 HasAuthentication interface.

---
+++ serenity-core/src/test/java/net/thucydides/core/webdriver/WhenUsingTheHasAuthenticationInterface.java
@@ -0,0 +1,37 @@
+package net.thucydides.core.webdriver;
+
+import net.thucydides.core.util.EnvironmentVariables;
+import net.thucydides.core.util.MockEnvironmentVariables;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.UsernameAndPassword;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.net.URI;
+import java.util.function.Predicate;
+
+public class WhenUsingTheHasAuthenticationInterface {
+
+    WebDriverFacade driver;
+    EnvironmentVariables environmentVariables;
+
+    @Before
+    public void initDriver() {
+        environmentVariables = new MockEnvironmentVariables();
+        environmentVariables.setProperty("headless.mode","true");
+        driver = new WebDriverFacade(ChromeDriver.class, new WebDriverFactory(), environmentVariables);
+    }
+
+    @Test
+    public void shouldAllowTheHasAuthenticationInterfaceIfTheDriverSupportsIt() {
+        driver.register(UsernameAndPassword.of("username", "pass"));
+    }
+
+    @Test
+    public void shouldAllowTheHasAuthenticationInterfaceUsingASupplierIfTheDriverSupportsIt() {
+        Predicate<URI> onlyAuthenticateOnTheLoginPage = uri -> uri.getPath().endsWith("/login");
+        driver.register(onlyAuthenticateOnTheLoginPage, UsernameAndPassword.of("username", "pass"));
+    }
+
+
+}

e643893d1bdb77c587d2cb3aa604d02e238db256
Liviu Carausu <liviu.carausu@gmail.com>
2023-03-19 09:56:33 +0100

    fixed lambda test tests for cucumber parallel execution (#3092)
    * Integrate serenity-jira-plugin
    
    * verify cucumber scenarios
    
    * verify cucumber scenarios
    
    * improve logging
    
    * improve test
    
    * fixed browser stack tests for cucumber parallel execution
    
    * fixed lambda test tests for cucumber parallel execution
    
    * fixed lambda test tests for cucumber parallel execution
    
    * improved lambda test tests for cucumber parallel execution
    
    * improved lambda test tests for cucumber parallel execution
    
    * improved lambda test tests for cucumber parallel execution
    
    ---------
    
    Co-authored-by: John Ferguson Smart <john.smart@wakaleo.com>

--- serenity-core/src/main/java/net/thucydides/core/steps/events/ExampleFinishedEvent.java
+++ serenity-core/src/main/java/net/thucydides/core/steps/events/ExampleFinishedEvent.java
@@ -1,16 +1,75 @@
 package net.thucydides.core.steps.events;
 
 
+import net.serenitybdd.core.webdriver.RemoteDriver;
+import net.thucydides.core.steps.session.TestSession;
+import net.thucydides.core.webdriver.SerenityWebdriverManager;
+import net.thucydides.core.webdriver.ThucydidesWebDriverSupport;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.SessionId;
+
 public class ExampleFinishedEvent extends StepEventBusEventBase {
 
-	public ExampleFinishedEvent() {}
+	private SessionId webSessionId;
+
+	private String driverUsedInThisTest;
+
+	private WebDriver webDriver;
+
+	public ExampleFinishedEvent() {
+		saveCurrentWebDriverContext();
+	}
+
+	private void saveCurrentWebDriverContext() {
+		WebDriver currentDriver = SerenityWebdriverManager.inThisTestThread().getCurrentDriver();
+		if (currentDriver != null) {
+			SessionId sessionId = RemoteDriver.of(currentDriver).getSessionId();
+			setWebSessionId(sessionId);
+			setWebDriver(currentDriver);
+			setDriverUsedInThisTest(ThucydidesWebDriverSupport.getDriversUsed());
+		}
+	}
 
 	@Override
 	public void play() {
+		if (getWebSessionId() != null) {
+			TestSession.getTestSessionContext().setWebSessionId(getWebSessionId());
+		}
+		if (getWebDriver() != null) {
+			TestSession.getTestSessionContext().setWebDriver(getWebDriver());
+		}
+		if (getDriverUsedInThisTest() != null) {
+			TestSession.getTestSessionContext().setDriverUsedInThisTest(getDriverUsedInThisTest());
+		}
 		getStepEventBus().exampleFinished();
 	}
 
 	public String toString() {
 		return("EventBusEvent EXAMPLE_FINISHED_EVENT ");
+	}
+
+
+	public SessionId getWebSessionId() {
+		return webSessionId;
+	}
+
+	public void setWebSessionId(SessionId webSessionId) {
+		this.webSessionId = webSessionId;
+	}
+
+	public String getDriverUsedInThisTest() {
+		return driverUsedInThisTest;
+	}
+
+	public void setDriverUsedInThisTest(String driverUsedInThisTest) {
+		this.driverUsedInThisTest = driverUsedInThisTest;
+	}
+
+	public WebDriver getWebDriver() {
+		return webDriver;
+	}
+
+	public void setWebDriver(WebDriver webDriver) {
+		this.webDriver = webDriver;
 	}
 }

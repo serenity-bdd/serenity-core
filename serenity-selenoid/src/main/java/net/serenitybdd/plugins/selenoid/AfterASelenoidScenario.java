3143af74af59908f498d902b9ee6d7e64ec505ec
kayle <1109481432@qq.com>
2023-07-16 17:16:11 +0800

    feat: Integrate selenoid

---
+++ serenity-selenoid/src/main/java/net/serenitybdd/plugins/selenoid/AfterASelenoidScenario.java
@@ -0,0 +1,18 @@
+package net.serenitybdd.plugins.selenoid;
+
+import net.serenitybdd.core.webdriver.RemoteDriver;
+import net.serenitybdd.core.webdriver.enhancers.AfterAWebdriverScenario;
+import net.thucydides.core.model.TestOutcome;
+import net.thucydides.core.util.EnvironmentVariables;
+import org.openqa.selenium.WebDriver;
+
+public class AfterASelenoidScenario implements AfterAWebdriverScenario {
+
+    @Override
+    public void apply(EnvironmentVariables environmentVariables, TestOutcome testOutcome, WebDriver driver) {
+        if ((driver == null) || (!RemoteDriver.isARemoteDriver(driver))) {
+            return;
+        }
+
+    }
+}

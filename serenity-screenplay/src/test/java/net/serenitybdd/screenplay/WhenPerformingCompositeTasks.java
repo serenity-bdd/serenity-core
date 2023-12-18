3af728f34c1c5e1c1ec804b428c0929623af4544
John Ferguson Smart <john.smart@wakaleo.com>
2023-12-18 18:29:44 +0000

    Minor test refactoring

---
+++ serenity-screenplay/src/test/java/net/serenitybdd/screenplay/WhenPerformingCompositeTasks.java
@@ -0,0 +1,58 @@
+package net.serenitybdd.screenplay;
+
+import net.serenitybdd.screenplay.shopping.BitesTheBanana;
+import net.serenitybdd.screenplay.shopping.ChewsTheBanana;
+import net.serenitybdd.screenplay.shopping.PeelABanana;
+import net.thucydides.core.steps.BaseStepListener;
+import net.thucydides.model.domain.TestResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import java.io.File;
+import java.nio.file.Files;
+import net.thucydides.core.steps.StepEventBus;
+
+public class WhenPerformingCompositeTasks {
+
+    private File temporaryDirectory;
+    private BaseStepListener listener;
+
+    @BeforeEach
+    public void setup() throws Exception {
+        temporaryDirectory = Files.createTempDirectory("tmp").toFile();
+        temporaryDirectory.deleteOnExit();
+        listener = new BaseStepListener(temporaryDirectory);
+
+        StepEventBus.getEventBus().clear();
+        StepEventBus.getEventBus().registerListener(listener);
+        StepEventBus.getEventBus().testStarted("some test");
+    }
+
+    @Test
+    public void aCompositeTaskShouldExecuteAllOfTheNestedTasks() {
+        // Given
+        Performable eatsABanana = new PeelABanana().then(new BitesTheBanana()).then(new ChewsTheBanana());
+
+        // When
+        Actor.named("Eddie").attemptsTo(eatsABanana);
+
+        // Then
+        testPassed();
+        testOutcomeContainsStep("Eddie peels a banana");
+        testOutcomeContainsStep("Eddie bites the banana");
+        testOutcomeContainsStep("Eddie chews the banana");
+    }
+
+    private void testPassed() {
+        Assertions.assertEquals(TestResult.SUCCESS, listener.latestTestOutcome().get().getResult());
+    }
+
+    private void testOutcomeContainsStep(String expectedDescription) {
+        Assertions.assertTrue(listener.latestTestOutcome().get().getTestSteps().stream()
+                .anyMatch(step -> step.getDescription().equals(expectedDescription)));
+    }
+
+    private void testOutcomeContainsNoSteps() {
+        Assertions.assertTrue(listener.latestTestOutcome().get().getTestSteps().isEmpty());
+    }
+}

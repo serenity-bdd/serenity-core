45f5a4d67e81f9e7218e325e2f80e820bff4f16b
Jan Molak <1089173+jan-molak@users.noreply.github.com>
2023-12-28 13:44:07 +0100

    Added more examples to demonstrate the issue

--- serenity-model/src/test/java/net/thucydides/model/requirements/AggregateRequirementsTest.java
+++ serenity-model/src/test/java/net/thucydides/model/requirements/AggregateRequirementsTest.java
@@ -45,6 +45,57 @@ class AggregateRequirementsTest {
         }
     }
 
+    @Test
+    void should_treat_files_in_a_single_level_directory_structure_as_representing_capabilities_and_features() {
+
+        List<Requirement> requirements = requirementsFrom(pathTo("serenity-js/spec-1-level"));
+
+        System.out.println(requirements);
+
+        assertThat(requirements).hasSize(1);
+
+        Requirement capability = requirements.get(0);
+        assertThat(capability.getName()).isEqualTo("payments");
+        assertThat(capability.getDisplayName()).isEqualTo("Payments");
+        assertThat(capability.getType()).isEqualTo("capability");
+
+        assertThat(capability.getChildren()).hasSize(1);
+
+        Requirement feature = capability.getChildren().get(0);
+        assertThat(feature.getName()).isEqualTo("card_payment");
+        assertThat(feature.getDisplayName()).isEqualTo("Card payment");
+        assertThat(feature.getType()).isEqualTo("feature");
+    }
+
+    @Test
+    void should_treat_files_in_a_two_level_directory_structure_as_representing_themes_capabilities_and_features() {
+
+        List<Requirement> requirements = requirementsFrom(pathTo("serenity-js/spec-2-levels"));
+
+        System.out.println(requirements);
+
+        assertThat(requirements).hasSize(1);
+
+        Requirement theme = requirements.get(0);
+        assertThat(theme.getName()).isEqualTo("ecommerce");
+        assertThat(theme.getDisplayName()).isEqualTo("Ecommerce");
+        assertThat(theme.getType()).isEqualTo("theme");
+
+        assertThat(theme.getChildren()).hasSize(1);
+
+        Requirement capability = theme.getChildren().get(0);
+        assertThat(capability.getName()).isEqualTo("payments");
+        assertThat(capability.getDisplayName()).isEqualTo("Payments");
+        assertThat(capability.getType()).isEqualTo("capability");
+
+        assertThat(capability.getChildren()).hasSize(1);
+
+        Requirement feature = capability.getChildren().get(0);
+        assertThat(feature.getName()).isEqualTo("card_payment");
+        assertThat(feature.getDisplayName()).isEqualTo("Card payment");
+        assertThat(feature.getType()).isEqualTo("feature");
+    }
+
     private List<Requirement> requirementsFrom(Path exampleRootDirectory) {
 
         Path requirementsDirectory = exampleRootDirectory.resolve("spec");

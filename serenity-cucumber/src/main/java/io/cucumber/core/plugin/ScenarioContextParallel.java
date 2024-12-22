8006229382b278ca97ad3aaf8dfd2a5be4ad29ff
Liviu Carausu <liviu.carausu@gmail.com>
2024-12-22 19:40:21 +0100

    #possible fix for https://github.com/serenity-bdd/serenity-core/issues/3546

--- serenity-cucumber/src/main/java/io/cucumber/core/plugin/ScenarioContextParallel.java
+++ serenity-cucumber/src/main/java/io/cucumber/core/plugin/ScenarioContextParallel.java
@@ -1,6 +1,8 @@
 package io.cucumber.core.plugin;
 
 
+import io.cucumber.plugin.event.Status;
+import net.serenitybdd.cucumber.events.StepFinishedWithResultEvent;
 import net.serenitybdd.model.exceptions.SerenityManagedException;
 import net.thucydides.core.steps.events.StepEventBusEvent;
 import io.cucumber.messages.types.Scenario;
@@ -334,6 +336,12 @@ public class ScenarioContextParallel {
 
     public void addStepEventBusEvent(StepEventBusEvent event) {
         if (TestSession.isSessionStarted()) {
+            if (event instanceof StepFinishedWithResultEvent) {
+                if (Status.FAILED.equals(((StepFinishedWithResultEvent) event).getResult().getStatus()) && TestSession.currentStepHasFailed()) {
+                    LOGGER.debug("SRP:ignored event " + event + " " + Thread.currentThread() + " because current Step has already failed");
+                    return;
+                }
+            }
             TestSession.addEvent(event);
             event.setStepEventBus(stepEventBus);
         } else {
